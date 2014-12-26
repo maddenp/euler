57c4bfdf76352d18e25023d1b2159840a246301a
Paul Madden <maddenp@colorado.edu>
2014-12-25 18:42:57 -0700

    add p/005.js

---
+++ p/005.js
@@ -0,0 +1,18 @@
+/*
+ *  Long version:
+ *
+ *  n=2520;
+ *  m=2520;
+ *  for (d=11;d<21;d++) {
+ *    while (true) {
+ *      if (n%d==0) break;
+ *      n+=m;
+ *    }
+ *    m=n;
+ *  }
+ *
+ */
+
+for (n=m=2520,d=11;d<21;d++,m=n) for (;n%d!=0;n+=m);
+
+console.log(n);
