93dd63fd5a8c8dd8c48e9d1124650fcdf8b0542e
Paul Madden <maddenp@colorado.edu>
2014-12-25 23:09:58 -0700

    add p/006.js

---
+++ p/006.js
@@ -0,0 +1,6 @@
+n=100;
+sum_of_naturals=n*(n+1)/2;
+square_of_sum_of_naturals=sum_of_naturals*sum_of_naturals;
+sum_of_squares=0;
+for (i=1;i<=100;i++) sum_of_squares+=i*i;
+console.log(square_of_sum_of_naturals-sum_of_squares);
