a6c4813535aa3474e85cc6aea61c085f3e3b93e3
bud manstrong <nbpeth@gmail.com>
2015-01-02 16:25:58 -0600

    No sieve this time, but I'll come back to it.

---
+++ n/js/007.js
@@ -0,0 +1,18 @@
+var primeCount = 0
+function isPrime(number){
+
+	for(i=2; i<number; i++){
+		if(number%i==0){
+			return;
+		}
+
+	}
+	primeCount ++
+
+}
+var prime;
+for(j=2; primeCount<10001; j++){
+	isPrime(j)
+	prime = j
+}
+console.log(prime)
