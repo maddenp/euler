fa4b06a8b398c7dd893e85b132d803c457390c2f
bud manstrong <nbpeth@gmail.com>
2015-01-03 11:25:41 -0600

    because science! and maths! And SPEED

--- n/js/009.js
+++ n/js/009.js
@@ -1,6 +1,6 @@
 for(c=3; c<1000; c++){
-	for(b=2; b<1000; b++){
-		for(a=1; a<1000; a++){
+	for(b=2; b<c; b++){
+		for(a=1; a<b; a++){
 			if((validateTriplet(a,b,c)==true) && a+b+c==1000){
 				console.log(a*b*c)
 				return;
