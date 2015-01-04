c6b7601c727e52ba93495ca7b697c8e5e1326f28
Paul Madden <maddenp@colorado.edu>
2015-01-04 11:08:25 -0700

    better variable name

--- p/010.js
+++ p/010.js
@@ -1,7 +1,7 @@
 x=2000000;
-for (sieve=[],sum=0,i=2;i<x;i++) {
-  if (sieve[i]) continue;
+for (composite=[],sum=0,i=2;i<x;i++) {
+  if (composite[i]) continue;
   sum+=i;
-  for (j=i+i;j<x;j+=i) sieve[j]=1;
+  for (j=i+i;j<x;j+=i) composite[j]=1;
 }
 console.log(sum);
