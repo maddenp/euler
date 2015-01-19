c03adff615ee27e042a4eb532119a692bb7c3595
bud manstrong <nbpeth@gmail.com>
2015-01-19 11:59:50 -0600

    because who needs to reset the triangle number and readd everything every time, am I right?

--- n/js/012.js
+++ n/js/012.js
@@ -3,20 +3,14 @@ var triangleNumber = 0
 var divisors = 0
 
 for(var i=1; divisors<250; i++){
-	triangleNumber = 0
 	triangle.push(i)
-
-	for(var j=0; j<triangle.length; j++){
-		triangleNumber += triangle[j]
-		
-	}
+	triangleNumber += triangle[i-1]
 
 	divisors = 0
 	for(var x=0; x<=Math.sqrt(triangleNumber); x++){
 		if(triangleNumber % x == 0){
 			divisors++
 		}
-
 	}
 }
 
