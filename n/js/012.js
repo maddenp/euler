3a0e316bf73ce91f3ade7b0dda4584b6757b080c
Paul Madden <maddenp@colorado.edu>
2015-01-19 11:55:38 -0700

    update nick's 012 -- replace array with integer

--- n/js/012.js
+++ n/js/012.js
@@ -1,20 +1,15 @@
-triangle = []
-var triangleNumber = 0
+var triangleNumber = 1
 var divisors = 0
 
-for(var i=1; divisors<250; i++){
-	triangle.push(i)
-	triangleNumber += triangle[i-1]
-
+for(var i=2; divisors<250; i++){
 	divisors = 0
 	for(var x=0; x<=Math.sqrt(triangleNumber); x++){
 		if(triangleNumber % x == 0){
 			divisors++
 		}
 	}
+	triangleNumber += i
+
 }
 
 console.log(triangleNumber)
-
-
-
