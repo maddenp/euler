fbf990f3d3eb8b3e0d8cdc64c1d34ac3b1bafd22
Paul Madden <maddenp@colorado.edu>
2015-08-10 21:47:49 -0600

    add p/js/016.js

---
+++ p/js/016.js
@@ -0,0 +1,23 @@
+var double_array = function(a) {
+  var carry = false;
+  for (var i = a.length-1; i >= 0; i--) {
+    a[i] *= 2;
+    if (carry) {
+      a[i] += 1;
+      carry = false;
+    }
+    if (a[i] > 9) {
+      a[i] -= 10;
+      if (i == 0) {
+        a.unshift(1);
+      } else {
+        carry = true;
+      }
+    }
+  }
+};
+
+a = [1];
+for (var i = 0; i < 1000; i++) double_array(a);
+
+console.log(a.reduce(function(m,e) { return m + e; }));
