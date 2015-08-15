bbaf74306808ebeaa87eeef15d0cd72f2e15d7ee
Paul Madden <maddenp@colorado.edu>
2015-08-15 13:39:23 -0600

    bad algorithm: should trax max_path_sum, not weight

--- p/js/018.js
+++ p/js/018.js
@@ -1,23 +1,23 @@
 "use strict";
 
-//var pyramid =
-//[
-//                            75,
-//                          95, 64,
-//                        17, 47, 82,
-//                      18, 35, 87, 10,
-//                    20,  4, 82, 47, 65,
-//                  19,  1, 23, 75,  3, 34,
-//                88,  2, 77, 73,  7, 63, 67,
-//              99, 65,  4, 28,  6, 16, 70, 92,
-//            41, 41, 26, 56, 83, 40, 80, 70, 33,
-//          41, 48, 72, 33, 47, 32, 37, 16, 94, 29,
-//        53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14,
-//      70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57,
-//    91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48,
-//  63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31,
-//  4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23
-//];
+var pyramid =
+[
+                             75,
+                           95, 64,
+                         17, 47, 82,
+                       18, 35, 87, 10,
+                     20,  4, 82, 47, 65,
+                   19,  1, 23, 75,  3, 34,
+                 88,  2, 77, 73,  7, 63, 67,
+               99, 65,  4, 28,  6, 16, 70, 92,
+             41, 41, 26, 56, 83, 40, 80, 70, 33,
+           41, 48, 72, 33, 47, 32, 37, 16, 94, 29,
+         53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14,
+       70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57,
+     91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48,
+   63, 66,  4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31,
+   4, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60,  4, 23
+];
 
 var pyramid = [3,7,4,2,4,6,8,5,9,3];
 
@@ -31,23 +31,25 @@ var Tree = function(pyramid,index,level,parent) {
       this.l = parent.l.r;
     } else {
       this.l = new Tree(pyramid, lindex, level+1, this);
-      this.weight += this.l.weight;
     }
+    this.weight += this.l.weight;
   }
   if (pyramid[rindex]) {
     this.r = new Tree(pyramid, rindex, level+1, this);
     this.weight += this.r.weight;
   }
+  console.log("### n: "+this.n+" lweight: "+(this.l ? this.l.weight : 0)+" rweight: "+(this.r ? this.r.weight : 0));
 };
 
 var p = new Tree(pyramid,0,1,null);
 
-var max_path_sum = p.n;
+var max_path_sum = 0;
 
 while (p) {
   max_path_sum += p.n;
   var lweight = p.l ? p.l.weight : 0;
   var rweight = p.r ? p.r.weight : 0;
+  console.log("n: "+p.n+" sum: "+max_path_sum+" lweight: "+lweight+" rweight: "+rweight);
   p = lweight >= rweight ? p.l : p.r;
 }
 
