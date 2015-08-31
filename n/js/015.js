8eb432334564d66e86024292bc20d4f5c277e638
bud manstrong <nbpeth@gmail.com>
2015-08-30 19:25:50 -0500

    because ternary

--- n/js/015.js
+++ n/js/015.js
@@ -4,16 +4,10 @@ for(j=0; j<gridHeight*2; j++){
 	currentArray = new Array(previousArray.length + 1)
 
 	for(i=0; i<previousArray.length + 1; i++){
-		if(i==0){
-			currentArray[i] = 1
-		}	
-		else if(i==previousArray.length){
-			currentArray[i] = 1
-		}
-		else{
-			currentArray[i] = previousArray[i-1] + previousArray[i]
-		}
+
+		currentArray[i] = (i==0 || i==previousArray.length) ? 1 : previousArray[i-1] + previousArray[i]
 	}
+
 	previousArray = currentArray
 }
 
