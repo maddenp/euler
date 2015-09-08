fa98759458af51f9f9438fb08ad92c10711637b5
Paul Madden <maddenp@colorado.edu>
2015-09-07 18:44:12 -0600

    add p/js/023.js

---
+++ p/js/023.js
@@ -0,0 +1,50 @@
+/* jshint node: true */
+
+"use strict";
+
+function abundant_numbers(limit) {
+  var ans = [];
+  for (var n = 1; n <= limit; n++) {
+    if (sum_of_proper_divisors(n) > n) {
+      ans.push(n);
+    }
+  }
+  return ans;
+}
+
+function array_sum(a) {
+  return (a.reduce(function(m, e) { return m + e; }, 0));
+}
+
+function proper_divisors(n) {
+  var pds = [1];
+  for (var d = 2; d < n; d++) {
+    if (n % d === 0) {
+      pds.push(d);
+    }
+  }
+  return pds;
+}
+
+function sum_of_proper_divisors(n) {
+  return array_sum(proper_divisors(n));
+}
+
+var limit = 28123;
+var ans = abundant_numbers(limit);
+
+var sieve = [];
+for (var i = 0; i < ans.length; i++) {
+  for (var j = i; j < ans.length; j++) {
+    var sum_of_ans = ans[i] + ans[j];
+    sieve[sum_of_ans] = true;
+  }
+}
+
+var sum = 0;
+for (var i = 1; i <= limit; i++) {
+  if (!sieve[i]) {
+    sum += i;
+  }
+}
+console.log(sum);
