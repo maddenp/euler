d7b2bb1a3e2d0cb8fd0967c1e4e2fd36682d8c66
Paul Madden <maddenp@colorado.edu>
2015-09-18 12:07:24 -0600

    simplify/speedup p/js/021.js

--- p/js/021.js
+++ p/js/021.js
@@ -2,43 +2,23 @@
 
 "use strict";
 
-function array_sum(a) {
-  return (a.reduce(function(m,e) { return m + e; }, 0));
-}
-
-function maybe_add(a, x, limit) {
-  if (x < limit && amicable_numbers.indexOf(x) === -1) {
-    a.push(x);
-  }
-}
-
-function proper_divisors(n) {
-  var pds = [1];
+function sum_of_proper_divisors(n) {
+  var sopd = 1;
   for (var d = 2; d <= Math.sqrt(n); d++) {
     if (n % d === 0) {
-      pds.push(d);
-      if (n/d !== d) {
-        pds.push(n/d);
-      }
+      sopd += d;
+      if (n/d !== d) sopd += n/d;
     }
   }
-  return pds;
+  return sopd;
 }
 
-function sum_of_proper_divisors(n) {
-  return array_sum(proper_divisors(n));
-}
-
-var limit = 10000;
-var amicable_numbers = [];
-
-for (var n = 1; n < limit; n++) {
+for (var n = 1, sum = 0; n < 10000; n++) {
   var s = sum_of_proper_divisors(n);
   var m = sum_of_proper_divisors(s);
   if (m === n && s !== n) {
-    maybe_add(amicable_numbers, n, limit);
-    maybe_add(amicable_numbers, s, limit);
+    sum += n;
   }
 }
 
-console.log(array_sum(amicable_numbers));
+console.log(sum);
