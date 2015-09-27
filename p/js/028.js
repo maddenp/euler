e8057246c85f116567caecc3754edb3a657db2ce
Paul Madden <maddenp@colorado.edu>
2015-09-27 13:43:29 -0500

    Add p/js/028.js & update tests

---
+++ p/js/028.js
@@ -0,0 +1,12 @@
+/* jshint node: true */
+
+"use strict";
+
+for (var n=1, side=3, step=2, sum=1; side <= 1001; side += 2, step += 2) {
+  for (var i=0; i < 4; i++) {
+    n += step;
+    sum += n;
+  }
+}
+
+console.log(sum);
