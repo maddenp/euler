5f446eaa1142ef7d656b2f722f42c08b88620137
Paul Madden <maddenp@colorado.edu>
2015-10-25 13:25:55 -0600

    Comments for 032

--- p/js/032.js
+++ p/js/032.js
@@ -19,25 +19,37 @@ function ndigits(n) {
   return Math.floor(Math.log(n) / Math.log(10)) + 1;
 }
 
-function pandigital(f1, f2, p) {
-  if (ndigits(f1) + ndigits(f2) + ndigits(p) !== 9) return false;
-  var narray = (n2a(f1).concat(n2a(f2)).concat(n2a(p))).sort();
+function pandigital(n, m, p) {
+  if (ndigits(n) + ndigits(m) + ndigits(p) !== 9) return false;
+  var narray = (n2a(n).concat(n2a(m)).concat(n2a(p))).sort();
   for (var i = 0; i < narray.length; i++) {
     if (narray[i] !== [1, 2, 3, 4, 5, 6, 7, 8, 9][i]) return false;
   }
   return true;
 }
 
-function pp_find(f1_lo, f1_hi, f2_lo, f2_hi, pp) {
-  for (var f1 = f1_lo; f1 < f1_hi; f1++) {
-    for (var f2 = f2_lo; f2 < f2_hi; f2++) {
-      var p = f1 * f2;
-      if (pandigital(f1, f2, p) && pp.indexOf(p) === -1) pp.push(p);
+function pp_find(n_lo, n_hi, m_lo, m_hi, pp) {
+  for (var n = n_lo; n < n_hi; n++) {
+    for (var m = m_lo; m < m_hi; m++) {
+      var p = n * m;
+      if (pandigital(n, m, p) && pp.indexOf(p) === -1) pp.push(p);
     }
   }
 }
 
 var pp = [];
+
+/* The product of an n-digit number and an m-digit number can have as few as
+ * n + m - 1 digits, and as many as n + m digits. For example, the product of
+ * the smallest 2-digit number (10) and the smallest 3-digit number (100) is
+ * 1000, with 4 digits; and the product of the largest 2-digit number (99) and
+ * the largest 3-digit number (999) is 98901, with 5 digits. In this case, the
+ * sum of n + m + p (where p is the number of digits in n * m), will either be
+ * 9 or 10. Since we require that n + m + p = 9, there are only two (n, m) pairs
+ * worth considering: (1, 4), with the n-digit number ranging from 1 to 9 and
+ * the m-digit number ranging from 1000 to 9999; and (2, 3), with the n-digit
+ * number ranging from 10 to 99 and the m-digit number ranging from 100 to 9999.
+ */
 
 pp_find(1, 10, 1000, 10000, pp);
 pp_find(10, 100, 100, 1000, pp);
