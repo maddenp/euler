104294bc22e0e5f6c1fb2245ec3f186287799d7f
Paul Madden <maddenp@colorado.edu>
2015-11-04 16:43:48 -0700

    Hardcode some computed values for speed

--- p/js/034.js
+++ p/js/034.js
@@ -8,15 +8,9 @@ function sum_of_factorialized_digits(n, factorials) {
   return pm.array_sum(pm.n2a(n).map(function(n) { return factorials[n]; }));
 }
 
-var factorials = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(pm.factorial);
-var limit = 9;
-
-while (pm.ndigits(sum_of_factorialized_digits(limit, factorials)) >= pm.ndigits(limit)) {
-  limit = limit * 10 + 9;
-}
-limit = Math.floor(limit / 10);
-
 var digit_factorials = [];
+var factorials = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(pm.factorial);
+var limit = 2540160; // 7 * 9!
 
 for (var n = 3; n <= limit; n++) {
   if (sum_of_factorialized_digits(n, factorials) === n) {
