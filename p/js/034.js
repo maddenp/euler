04a88de517927b22ff36c37c62fd912821695044
Paul Madden <maddenp@colorado.edu>
2015-11-04 17:20:59 -0700

    Replace comment with code

--- p/js/034.js
+++ p/js/034.js
@@ -10,7 +10,7 @@ function sum_of_factorialized_digits(n, factorials) {
 
 var digit_factorials = [];
 var factorials = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(pm.factorial);
-var limit = 2540160; // 7 * 9!
+var limit = 7 * pm.factorial(9);
 
 for (var n = 3; n <= limit; n++) {
   if (sum_of_factorialized_digits(n, factorials) === n) {
