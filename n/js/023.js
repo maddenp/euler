40d7bb8f58aa7a50f6349844f7c1e2a790ae7f4a
bud manstrong <nbpeth@gmail.com>
2015-11-08 15:32:33 -0600

    make that shit sparkle. more elegant design.

--- n/js/023.js
+++ n/js/023.js
@@ -1,13 +1,7 @@
-
-
-var halfsies = 0
-var abundantNumbers = []
 var max = 28123
-var theOnesWeAreLookingFor = []
-
-findAllTheAbundantNumbers()
 
 function findAllTheAbundantNumbers(){
+	var abundantNumbers = []
 	for(var x=1; x<=max; x++){
 		var divisors = findDivisors(x)
 		var sumOfDivisors = 0
@@ -18,13 +12,13 @@ function findAllTheAbundantNumbers(){
 			abundantNumbers.push(x)
 		}
 	}
+	return abundantNumbers
 }
 
 function findDivisors(num){
-	halfsies = Math.sqrt(num)
 	var divisors = [1]
-	for(b=2; b<=halfsies; b++){
-		if(num % b == 0 ){
+	for(b=2; b<=Math.sqrt(num); b++){
+		if(num % b == 0){
 			divisors.push(b)
 			if (num / b !== b) {
 		        divisors.push(num / b);
@@ -34,24 +28,37 @@ function findDivisors(num){
 	return divisors
 }
 
-for(var p=0; p<abundantNumbers.length; p++){
-	for(var f=0; f<abundantNumbers.length; f++){
-		var sum = abundantNumbers[p] + abundantNumbers[f]
-		if(sum <= max){
-			theOnesWeAreLookingFor[sum] = true
-		}
-		else{
-			break;
+function findTheOnesWeAreLookingFor(){
+	var abundantNumbers = findAllTheAbundantNumbers(max)
+
+	var theOnesWeAreLookingFor = []
+	for(var p=0; p<abundantNumbers.length; p++){
+		for(var f=0; f<abundantNumbers.length; f++){
+			var sum = abundantNumbers[p] + abundantNumbers[f]
+			if(sum <= max){
+				theOnesWeAreLookingFor[sum] = true
+			}
+			else{
+				break;
+			}
 		}
 	}
+	return theOnesWeAreLookingFor
 }
-var sum = 0
-for(var k=0; k<theOnesWeAreLookingFor.length; k++){
-	if(!theOnesWeAreLookingFor[k]){
-		sum += k
+
+function addEmUp(){
+	var theOnesWeAreLookingFor = findTheOnesWeAreLookingFor()
+	var sum = 0
+	for(var k=0; k<theOnesWeAreLookingFor.length; k++){
+		if(!theOnesWeAreLookingFor[k]){
+			sum += k
+		}
 	}
+	console.log(sum)
 }
-console.log(sum)
+
+addEmUp()
+
 
 
 
