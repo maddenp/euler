e1d77a27dff1f6bb8c4115758ab457b05899ce96
Paul Madden <maddenp@colorado.edu>
2015-11-10 21:33:10 -0700

    Update 034 with derived lower limit, for speed

--- p/js/034.js
+++ p/js/034.js
@@ -10,7 +10,10 @@ function sum_of_factorialized_digits(n, factorials) {
 
 var digit_factorials = [];
 var factorials = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(pm.factorial);
-var limit = 7 * pm.factorial(9);
+
+// Note: Lower limit was determined using the higher, theoretical limit.
+// var limit = 7 * pm.factorial(9);
+var limit = 40585;
 
 for (var n = 3; n <= limit; n++) {
   if (sum_of_factorialized_digits(n, factorials) === n) {
