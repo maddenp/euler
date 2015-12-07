8f4c84d11875ad1aa6d713e2773cf4ccf352a30d
Paul Madden <maddenp@colorado.edu>
2015-12-07 11:30:10 -0700

    Add p/js/039.js & update tests

---
+++ p/js/039.js
@@ -0,0 +1,29 @@
+/* jshint node: true */
+
+"use strict";
+
+var pm = require('./pm');
+
+var solutions = [];
+var limit = 1000;
+
+for (var i = 0; i <= limit; i++) solutions[i] = 0;
+
+for (var a = 1; a <= limit - 2; a++) {
+  for (var b = a; b <= limit - a; b++) {
+    var c = pm.hypotenuse(a, b);
+    var perimeter = a + b + c;
+    if (pm.integral(perimeter) && perimeter <= limit) {
+      ++solutions[perimeter];
+    }
+  }
+}   
+
+for (var perimeter = 0, max = 0, i = 1; i <= limit; i++) {
+  if (solutions[i] > max) {
+    max = solutions[i];
+    perimeter = i;
+  }
+}
+
+console.log(perimeter);
