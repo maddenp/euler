076278eb50c13ed6053e1b8827e8d017945cca08
Paul Madden <maddenp@colorado.edu>
2016-01-07 20:06:34 -0700

    Finish refactoring from previous commit

--- p/js/044.js
+++ p/js/044.js
@@ -4,11 +4,6 @@
 
 var pm = require('./pm');
 
-function is_pentagon_number(n) {
-  var test = function(n) { return pm.is_integral(n) && n > 0; };
-  return pm.quadratic_roots(3, -1, -n * 2).filter(test).length > 0;
-}
-
 function pentagon_number(n) {
   return (n * (3 * n - 1)) / 2;
 }
@@ -22,7 +17,7 @@ out: while (true) {
   for (var i = 0; i < last; i++) {
     var diff = pn[last] - pn[i];
     var sum  = pn[last] + pn[i];
-    if (is_pentagon_number(diff) && is_pentagon_number(sum)) {
+    if (pm.is_pentagon_number(diff) && pm.is_pentagon_number(sum)) {
       console.log(pn[last] - pn[i]);
       break out;
     }
