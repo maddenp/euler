176ab8a1ca6c9d1ff4814916c59d50d6ca7c558a
Paul Madden <maddenp@colorado.edu>
2016-01-16 00:09:19 -0700

    050 down to 0.5s vs ~30s for first draft

--- p/js/050.js
+++ p/js/050.js
@@ -7,31 +7,21 @@ var pm = require('./pm');
 var limit = 1000000;
 var max_chain_len = 0;
 var max_prime = 0;
-var sums = [];
-
 var primes_and_is_prime = pm.primes(1, limit, true);
 var primes = primes_and_is_prime[0];
 var is_prime = primes_and_is_prime[1];
 
-for (var i = 0, sum = 0; i < primes.length; i++) {
-  sum += primes[i];
-  sums[i] = sum;
-}
-
-outer: for (var i = 0; i < sums.length - 1; i++) {
-  var offset = i > 0 ? sums[i - 1] : 0;
-  for (var j = sums.length - 1; j >= i; j--) {
-    var chain_len = j - i;
-    if (chain_len <= max_chain_len) {
-      continue outer;
-    }
-    var n = sums[j] - offset;
-    if (n <= limit && is_prime[n]) {
+outer: for (var i = 0; i < primes.length - 2; i++) {
+  var sum = primes[i];
+  for (var j = i + 1; j < primes.length - 1; j++) {
+    sum += primes[j];
+    if (sum > limit) continue outer;
+    if (is_prime[sum]) {
+      var chain_len = j - i;
       if (chain_len > max_chain_len) {
         max_chain_len = chain_len;
-        max_prime = n;
+        max_prime = sum;
       }
-      continue outer;
     }
   }
 }
