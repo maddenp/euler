5b2d3828cf262fbfc5789b03292c914528d8c66f
Paul Madden <maddenp@colorado.edu>
2016-01-15 23:39:47 -0700

    Optimization in 050, still slow

--- p/js/050.js
+++ p/js/050.js
@@ -13,33 +13,24 @@ var primes_and_is_prime = pm.primes(1, limit, true);
 var primes = primes_and_is_prime[0];
 var is_prime = primes_and_is_prime[1];
 
-function downshift(i, j, sums) {
-  if (i > 0) {
-    for (var k = j - 1; k >= i; k--) {
-      sums[k] -= sums[i - 1];
-    }
-  }
-}
-
 for (var i = 0, sum = 0; i < primes.length; i++) {
   sum += primes[i];
   sums[i] = sum;
 }
 
 outer: for (var i = 0; i < sums.length - 1; i++) {
+  var offset = i > 0 ? sums[i - 1] : 0;
   for (var j = sums.length - 1; j >= i; j--) {
     var chain_len = j - i;
-    if (i > 0) sums[j] -= sums[i - 1];
     if (chain_len <= max_chain_len) {
-      downshift(i, j, sums);
       continue outer;
     }
-    if (sums[j] <= limit && is_prime[sums[j]]) {
+    var n = sums[j] - offset;
+    if (n <= limit && is_prime[n]) {
       if (chain_len > max_chain_len) {
         max_chain_len = chain_len;
-        max_prime = sums[j];
+        max_prime = n;
       }
-      downshift(i, j, sums);
       continue outer;
     }
   }
