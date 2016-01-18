7f6b20f1e1a34ca34c301b03a38d351c10155734
Paul Madden <maddenp@colorado.edu>
2016-01-18 14:38:54 -0700

    Tweak 012.js

--- p/js/012.js
+++ p/js/012.js
@@ -2,12 +2,12 @@
 
 "use strict";
 
-var i, j, sum = 0, triangle_number = 1;
+var sum = 0, triangle_number = 1;
 
-for (i = 2; sum <= 250; i++) {
+for (var i = 2; sum <= 250; i++) {
   sum = 0;
-  var increment = triangle_number % 2 === 0 ? 1 : 2;
-  for (j = 1; j <= Math.sqrt(triangle_number); j += increment) {
+  var step = triangle_number % 2 === 0 ? 1 : 2;
+  for (var j = 1; j <= Math.sqrt(triangle_number); j += step) {
     if (triangle_number % j === 0) {
       ++sum;
     }
