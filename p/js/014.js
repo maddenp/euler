80d711b539b383ef576945332ca84e6c3c38d0fe
Paul Madden <maddenp@colorado.edu>
2016-01-19 12:54:11 -0700

    Improve 014 code

--- p/js/014.js
+++ p/js/014.js
@@ -1,34 +1,27 @@
 /* jshint node: true */
-/* jshint boss: true */
 
 "use strict";
 
-/*
- * Elegant but slower recursive solution:
- *
- *  function chainlen(n) {
- *    if (n === 1) return 1;
- *    if (k = known[n]) return k;
- *    return known[n] = 1 + chainlen((n % 2 === 0)?(n / 2):(3 * n + 1));
- *  }
- *
-*/
-
 function chainlen(n) {
-  var k, m, s;
-  if (k = known[n]) return k;
-  for (m = n,s = 1; m > 1; s++) {
-    m = (m % 2 === 0) ? (m / 2) : (3 * m + 1);
-    if (k = known[m]) { s += k; break; }
+  if (known[n]) return known[n];
+  for (var m = n, s = 1; m > 1; s++) {
+    m = (m & 1 === 1) ? (3 * m + 1) : (m / 2);
+    if (known[m]) {
+      s += known[m];
+      break;
+    }
   }
-  known[n] = s;
-  return s;
+  return known[n] = s;
 }
 
-var c, known, maxlen, maxn, n;
+var known = [], maxlen = 1, maxn = 1, n;
 
-for (known = [], maxlen = 1, maxn = 1, n = 1; n < 1000000; n++) {
-  if ((c = chainlen(n)) > maxlen) { maxlen = c; maxn = n; }
+for (n = 1; n < 1000000; n++) {
+  var c = chainlen(n);
+  if (c > maxlen) {
+    maxlen = c;
+    maxn = n;
+  }
 }
 
 console.log(maxn);
