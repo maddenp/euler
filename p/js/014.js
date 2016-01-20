1663576ef9a827b585ee495d20ea3009c4293ff0
Paul Madden <maddenp@colorado.edu>
2016-01-19 19:30:18 -0700

    There we go: A winning optimization for 014

--- p/js/014.js
+++ p/js/014.js
@@ -2,10 +2,18 @@
 
 "use strict";
 
+/*
+ * Follow the Collatz chain and tally the number of steps to reach 1. However,
+ * when encountering a chain element that has been seen before, make use of the
+ * value memoized for that element and follow the chain no further. Note that
+ * we will only have memoized elements lower than the current starting point,
+ * so guard an expensive array lookup with a cheap comparison.
+ */
+
 function chainlen(n) {
   for (var m = n, s = 1; m > 1; s++) {
     m = (m & 1 === 1) ? (3 * m + 1) : (m >>> 1);
-    if (known[m]) {
+    if (m < n && known[m]) {
       return (known[n] = s + known[m]);
     }
   }
