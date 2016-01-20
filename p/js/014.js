4d07c03379ec4b08dba4a3464acd7c5e9cc92f43
Paul Madden <maddenp@colorado.edu>
2016-01-19 19:13:16 -0700

    Bitwise and is a little faster than mod 2

--- p/js/014.js
+++ p/js/014.js
@@ -4,7 +4,7 @@
 
 function chainlen(n) {
   for (var m = n, s = 1; m > 1; s++) {
-    m = (m % 2 === 0) ? (m / 2) : (3 * m + 1);
+    m = (m & 1 === 1) ? (3 * m + 1) : (m / 2);
     if (known[m]) {
       s += known[m]
       break;
