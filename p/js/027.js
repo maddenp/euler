5ccc9247cf1ed2ebb7e36cec19e12ec9d769006a
Paul Madden <maddenp@colorado.edu>
2016-01-20 09:11:12 -0700

    Combine positive/negative variant checks in 027

--- p/js/027.js
+++ p/js/027.js
@@ -12,18 +12,37 @@ function is_non_prime(n) {
 var max = 0;
 var product = 0;
 
-for (var a = -999; a < 1000; a++) {
+for (var a = 0; a < 1000; a++) {
   for (var b = -999; b < 1000; b++) {
-    var count = 0;
+    var active_n = true;
+    var active_p = true;
+    var count_n = 0;
+    var count_p = 0;
     var n = 0;
-    while (true) {
-      if (is_non_prime(n * (n + a) + b)) break;
+    while (active_n || active_p) {
+      if (active_n) {
+        if (is_non_prime(n * (n - a) + b)) {
+          active_n = false;
+        } else {
+          ++count_n;
+        }
+      }
+      if (active_p) {
+        if (is_non_prime(n * (n + a) + b)) {
+          active_p = false;
+        } else {
+          ++count_p;
+        }
+      }
       ++n;
-      ++count;
     }
-    if (count > max) {
-      max = count;
-      product = a * b;
+    if (count_n > max) {
+      max = count_n;
+      product = -a * b;
+    }
+    if (count_p > max) {
+      max = count_p;
+      product = +a * b;
     }
   }
 }
