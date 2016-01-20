bb422098cf11f954686064afe851dd67dd39a70e
Paul Madden <maddenp@colorado.edu>
2016-01-20 09:44:35 -0700

    In 027, loop only over positive a and positive b

--- p/js/027.js
+++ p/js/027.js
@@ -13,39 +13,31 @@ var max = 0;
 var product = 0;
 
 for (var a = 0; a < 1000; a++) {
-  for (var b = -999; b < 1000; b++) {
-    var active_an = true;
-    var active_ap = true;
-    var count_an = 0;
-    var count_ap = 0;
+  for (var b = 0; b < 1000; b++) {
+    var active_an_bn = true, active_an_bp = true, active_ap_bn = true, active_ap_bp = true;
+    var count_an_bn = 0, count_an_bp = 0, count_ap_bn = 0, count_ap_bp = 0;
     var n = 0;
-    while (active_an || active_ap) {
+    while (active_an_bn || active_an_bp || active_ap_bn || active_ap_bp) {
       var m = n * n;
       var o = a * n;
-      if (active_an) {
-        if (is_non_prime(m - o + b)) {
-          active_an = false;
-        } else {
-          ++count_an;
-        }
+      if (active_an_bn) {
+        if (is_non_prime(m - o - b)) { active_an_bn = false; } else { ++count_an_bn; }
       }
-      if (active_ap) {
-        if (is_non_prime(m + o + b)) {
-          active_ap = false;
-        } else {
-          ++count_ap;
-        }
+      if (active_an_bp) {
+        if (is_non_prime(m - o + b)) { active_an_bp = false; } else { ++count_an_bp; }
+      }
+      if (active_ap_bn) {
+        if (is_non_prime(m + o - b)) { active_ap_bn = false; } else { ++count_ap_bn; }
+      }
+      if (active_ap_bp) {
+        if (is_non_prime(m + o + b)) { active_ap_bp = false; } else { ++count_ap_bp; }
       }
       ++n;
     }
-    if (count_an > max) {
-      max = count_an;
-      product = -a * b;
-    }
-    if (count_ap > max) {
-      max = count_ap;
-      product = +a * b;
-    }
+    if (count_an_bn > max) { max = count_an_bn; product = -a * -b; }
+    if (count_an_bp > max) { max = count_an_bp; product = -a * +b; }
+    if (count_ap_bn > max) { max = count_ap_bn; product = +a * -b; }
+    if (count_ap_bp > max) { max = count_ap_bp; product = +a * +b; }
   }
 }
 
