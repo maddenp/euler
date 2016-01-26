d18bf50a403560188f303520051c7535a3b5d3d0
Paul Madden <maddenp@colorado.edu>
2016-01-26 11:48:41 -0700

    ~350% speedup in 036

--- p/js/036.js
+++ p/js/036.js
@@ -4,19 +4,26 @@
 
 var pm = require('./pm');
 
-function a_is_palindrome(a) {
+function is_palindrome(a) {
   for (var i = 0; i < Math.floor(a.length / 2); i++) {
     if (a[i] !== a[a.length - (i + 1)]) return false;
   }
   return true;
 }
 
+function add_two(a) {
+  pm.array_inc(a);
+  pm.array_inc(a);
+}
+
+var a = [1];
 var double_base_palindromes = [];
 
 for (var n = 1; n < 1000000; n += 2) {
-  if (a_is_palindrome(pm.n2a(n)) && a_is_palindrome(pm.n2a_binary(n))) {
+  if (is_palindrome(a) && is_palindrome(pm.n2a_binary(n))) {
     double_base_palindromes.push(n);
   }
+  add_two(a);
 }
 
 console.log(pm.array_sum(double_base_palindromes));
