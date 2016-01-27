09f26ddf3bb6f5d51411bd767648cb1d0218a705
Paul Madden <maddenp@colorado.edu>
2016-01-27 09:12:16 -0700

    Make use of pm.primes() in 037

--- p/js/037.js
+++ p/js/037.js
@@ -4,17 +4,18 @@
 
 var pm = require('./pm');
 
-function truncatable(n, fn, prime) {
+function truncatable(n, fn, primes_map) {
   var a = pm.n2a(n);
   while (a.length > 0) {
-    if (! prime(pm.a2n(a))) return false;
+    if (! primes_map[pm.a2n(a)]) return false;
     a[fn]();
   }
   return true;
 }
 
-function truncatable_both_ways(n, prime) {
-  return truncatable(n, 'shift', prime) && truncatable(n, 'pop', prime);
+function truncatable_both_ways(n, primes_map) {
+  return truncatable(n, 'shift', primes_map) &&
+    truncatable(n, 'pop', primes_map);
 }
 
 var filename = '037.dat';
@@ -22,21 +23,17 @@ var guess_limit = 1000000;
 var tight_limit = pm.readfile(filename);
 var limit = tight_limit || guess_limit;
 
-var prime = function(limit) {
-  var primes = pm.primes_map(limit);
-  return function(n) {
-    return primes[n];
-  };
-}(limit);
+var primes_and_primes_map = pm.primes(11, limit, true);
+var primes = primes_and_primes_map[0];
+var primes_map = primes_and_primes_map[1];
 
 var truncatable_primes = [];
 
-for (var n = 11; n < limit; n += 2) {
-  if (prime(n)) {
-    if (truncatable_both_ways(n, prime)) {
-      truncatable_primes.push(n);
-      if (truncatable_primes.length === 11) break;
-    }
+for (var i = 0; i < primes.length; i++) {
+  var n = primes[i];
+  if (truncatable_both_ways(n, primes_map)) {
+    truncatable_primes.push(n);
+    if (truncatable_primes.length === 11) break;
   }
 }
 
