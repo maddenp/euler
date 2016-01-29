dbb3cbf1cb16ded69b62fe4a8c307a8fdcec856b
Paul Madden <maddenp@colorado.edu>
2016-01-28 18:47:19 -0700

    Remove 034 hint => terrible slowdown

--- p/js/034.js
+++ p/js/034.js
@@ -10,19 +10,12 @@ function sum_of_factorialized_digits(n, factorials) {
 
 var digit_factorials = [];
 var factorials = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(pm.factorial);
-var filename = '034.dat';
-var theoretical_limit = 7 * pm.factorial(9);
-var experimental_limit = pm.readfile(filename);
-var limit = experimental_limit || theoretical_limit;
+var limit = 7 * pm.factorial(9);
 
 for (var n = 3; n <= limit; n++) {
   if (sum_of_factorialized_digits(n, factorials) === n) {
     digit_factorials.push(n);
   }
-}
-
-if (! experimental_limit) {
-  pm.writefile(filename, pm.array_max(digit_factorials));
 }
 
 console.log(pm.array_sum(digit_factorials));
