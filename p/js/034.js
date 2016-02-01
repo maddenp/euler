394a8cf280125971f0a99a0d1d912e41961013f8
Paul Madden <maddenp@colorado.edu>
2016-02-01 11:51:42 -0700

    23.5x speedup in 034 vs baseline by not recomputing sum

--- p/js/034.js
+++ p/js/034.js
@@ -10,12 +10,17 @@ var limit = 7 * factorials[9];
 
 var a = [3];
 var f = a.map(function(x) { return factorials[x]; });
+var sum = pm.array_sum(f);
 
 for (var n = 3; n <= limit; n++) {
-  if (pm.array_sum(f) === n) digit_factorials.push(n);
+  if (sum === n) digit_factorials.push(n);
   var changed = pm.array_inc(a);
+  for (var i = 0; i < Math.min(changed, f.length); i++) {
+    sum -= f[f.length - 1 - i];
+  }
   for (var i = a.length - 1; i >= a.length - changed; i--) {
     f[i] = factorials[a[i]];
+    sum += f[i];
   }
 }
 
