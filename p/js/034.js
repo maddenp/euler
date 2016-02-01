be3df6d1b6f6d440574f2f0a0c21c82d66fe01f4
Paul Madden <maddenp@colorado.edu>
2016-02-01 12:01:06 -0700

    A little refactoring in 034.js

--- p/js/034.js
+++ p/js/034.js
@@ -13,14 +13,17 @@ var f = a.map(function(x) { return factorials[x]; });
 var sum = pm.array_sum(f);
 
 for (var n = 3; n <= limit; n++) {
-  if (sum === n) digit_factorials.push(n);
-  var changed = pm.array_inc(a);
-  for (var i = 0; i < Math.min(changed, f.length); i++) {
-    sum -= f[f.length - 1 - i];
+  if (sum === n) {
+    digit_factorials.push(n);
   }
-  for (var i = a.length - 1; i >= a.length - changed; i--) {
-    f[i] = factorials[a[i]];
-    sum += f[i];
+  var changed = pm.array_inc(a);
+  var f_length = f.length;
+  for (var i = 0; i < changed; i++) {
+    var offset = i + 1;
+    var j = a.length - offset;
+    sum -= i < f_length ? f[f_length - offset] : 0;
+    f[j] = factorials[a[j]];
+    sum += f[j];
   }
 }
 
