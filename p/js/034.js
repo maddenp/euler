b7735c286ecb819f1a318b54a7af9c4679d0ef17
Paul Madden <maddenp@colorado.edu>
2016-02-01 10:25:09 -0700

    2.9x speedup in 034 vs baseline by maintaining factorials array

--- p/js/034.js
+++ p/js/034.js
@@ -9,11 +9,14 @@ var factorials = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(pm.factorial);
 var limit = 7 * factorials[9];
 
 var a = [3];
+var f = a.map(function(x) { return factorials[x]; });
+
 for (var n = 3; n <= limit; n++) {
-  if (pm.array_sum(a.map(function(x) { return factorials[x]; })) === n) {
-    digit_factorials.push(n);
+  if (pm.array_sum(f) === n) digit_factorials.push(n);
+  var changed = pm.array_inc(a);
+  for (var i = a.length - 1; i >= a.length - changed; i--) {
+    f[i] = factorials[a[i]];
   }
-  pm.array_inc(a);
 }
 
 console.log(pm.array_sum(digit_factorials));
