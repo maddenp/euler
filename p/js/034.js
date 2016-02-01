d24191d501bd60bc610f4fa1de608649b5391a17
Paul Madden <maddenp@colorado.edu>
2016-02-01 10:05:03 -0700

    1.9x speedup in 034 vs baseline by keeping and incrementing array

--- p/js/034.js
+++ p/js/034.js
@@ -4,18 +4,16 @@
 
 var pm = require('./pm');
 
-function sum_of_factorialized_digits(n, factorials) {
-  return pm.array_sum(pm.n2a(n).map(function(n) { return factorials[n]; }));
-}
-
 var digit_factorials = [];
 var factorials = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(pm.factorial);
-var limit = 7 * pm.factorial(9);
+var limit = 7 * factorials[9];
 
+var a = [3];
 for (var n = 3; n <= limit; n++) {
-  if (sum_of_factorialized_digits(n, factorials) === n) {
+  if (pm.array_sum(a.map(function(x) { return factorials[x]; })) === n) {
     digit_factorials.push(n);
   }
+  pm.array_inc(a);
 }
 
 console.log(pm.array_sum(digit_factorials));
