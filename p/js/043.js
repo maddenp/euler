a9bf36aa298dc76c2e96ba9c61af2d60f4984951
Paul Madden <maddenp@colorado.edu>
2016-02-02 15:46:34 -0700

    3x speedup in 043 vs baseline by avoiding library calls like pm.a2n()

--- p/js/043.js
+++ p/js/043.js
@@ -5,14 +5,14 @@
 var pm = require('./pm');
 
 function test(a) {
-  if (a[0] === 0) return false;
-  if ((a[3] & 1) !== 0) return false;
-  if ((a[2] + a[3] + a[4]) % 3 !== 0) return false;
-  if (a[5] % 5 !== 0) return false;
-  if (pm.a2n(a.slice(4, 7)) % 7 !== 0) return false;
-  if (pm.a2n(a.slice(5, 8)) % 11 !== 0) return false;
-  if (pm.a2n(a.slice(6, 9)) % 13 !== 0) return false;
-  if (pm.a2n(a.slice(7, 10)) % 17 !== 0) return false;
+  if (a[0] === 0)                                 return false;
+  if ((a[3] & 1) !== 0)                           return false;
+  if ((a[2] + a[3] + a[4]) % 3 !== 0)             return false;
+  if (a[5] % 5 !== 0)                             return false;
+  if ((a[4] * 100 + a[5] * 10 + a[6]) % 7  !== 0) return false;
+  if ((a[5] * 100 + a[6] * 10 + a[7]) % 11 !== 0) return false;
+  if ((a[6] * 100 + a[7] * 10 + a[8]) % 13 !== 0) return false;
+  if ((a[7] * 100 + a[8] * 10 + a[9]) % 17 !== 0) return false;
   return true;
 }
 
