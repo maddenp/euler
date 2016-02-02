dcb0780c16d1f184d65c75b1ed3cc5ad19999eb5
Paul Madden <maddenp@colorado.edu>
2016-02-02 15:13:11 -0700

    1.7x speedup in 043 by unrolling loop and improving checks

--- p/js/043.js
+++ p/js/043.js
@@ -6,18 +6,21 @@ var pm = require('./pm');
 
 function test(a) {
   if (a[0] === 0) return false;
-  if (a[3] % 2 !== 0) return false;
+  if ((a[3] & 1) !== 0) return false;
+  if ((a[2] + a[3] + a[4]) % 3 !== 0) return false;
   if (a[5] % 5 !== 0) return false;
-  var divisors = [2, 3, 5, 7, 11, 13, 17];
-  for (var i = 7; i >= 1; i--) {
-    if (pm.a2n(a.slice(i, i + 3)) % divisors[i - 1] !== 0) {
-      return false;
-    }
-  }
+  if (pm.a2n(a.slice(4, 7)) % 7 !== 0) return false;
+  if (pm.a2n(a.slice(5, 8)) % 11 !== 0) return false;
+  if (pm.a2n(a.slice(6, 9)) % 13 !== 0) return false;
+  if (pm.a2n(a.slice(7, 10)) % 17 !== 0) return false;
   return true;
 }
 
 var a = pm.range(0, 9);
 var answers = [];
-pm.permutations(a.length, a, function(x) { if (test(x)) answers.push(pm.a2n(x)); });
+
+pm.permutations(a.length, a, function(x) {
+  if (test(x)) answers.push(pm.a2n(x))
+});
+
 console.log(pm.array_sum(answers));
