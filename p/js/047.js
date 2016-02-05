366f36005ed3219d07a1006ec107affcf3c644bf
Paul Madden <maddenp@colorado.edu>
2016-02-05 10:20:48 -0700

    2.6x speedup in 047 vs baseline

--- p/js/047.js
+++ p/js/047.js
@@ -4,15 +4,47 @@
 
 var pm = require('./pm');
 
-var chain = [], count = 4, primes = [];
-
-for (var n = 2; chain.length < count; n++) {
-  var unique_pfs = pm.uniques(pm.prime_factorization(n, primes));
-  if (unique_pfs.length === count) {
-    chain.push(n);
-    continue;
+function ordered_insert(a, n) {
+  if (a.length === 0) {
+    a.push(n);
+  } else {
+    for (var i = 0; i < a.length && a[i] <= n; i++) {
+      if (a[i] === n) return;
+    }
+    a.splice(i, 0, n);
   }
-  chain = [];
 }
 
-console.log(chain[0]);
+function prime_factors(n, primes, factors) {
+  var y = 1;
+  for (var i = Math.floor(Math.sqrt(n)); i >= 0; i--) {
+    if (primes[i] && n % i === 0) {
+      y = i;
+      break;
+    }
+  }
+  var f = [];
+  if (y > 1) {
+    f = factors[n / y].slice();
+    ordered_insert(f, y);
+  }
+  factors[n] = f;
+  if (f.length === 0) {
+    primes[n] = true;
+    factors[n] = [n];
+  }
+  return factors[n];
+};
+
+var chain_len = 0, factors = {}, goal = 4, primes = [];
+
+for (var n = 2; chain_len < goal; n++) {
+  var count = prime_factors(n, primes, factors).length;
+  if (count === goal) {
+    ++chain_len;
+  } else {
+    chain_len = 0;
+  }
+}
+
+console.log(n - 4);
