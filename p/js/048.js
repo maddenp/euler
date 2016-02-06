ecdb124623295ddc2e549c34a857e2569578c8eb
Paul Madden <maddenp@colorado.edu>
2016-02-06 10:09:53 -0700

    Refactoring in 048

--- p/js/048.js
+++ p/js/048.js
@@ -4,16 +4,12 @@
 
 var pm = require('./pm');
 
-function low_order_pow(multiplier, digits) {
-  var product = [1];
-  for (var i = 0; i < multiplier; i++) {
-    product = pm.array_trunc(pm.array_mult(product, multiplier), 10);
-  }
-  return product;
-}
-
 for (var n = 1, sum = [0]; n <= 1000; n++) {
-  sum = pm.array_add(sum, low_order_pow(n, 10));
+  var product = [1];
+  for (var i = 0; i < n; i++) {
+    product = pm.array_trunc(pm.array_mult(product, n), 10);
+  }
+  sum = pm.array_add(sum, product);
 }
 
 console.log(pm.a2n(pm.array_trunc(sum, 10)));
