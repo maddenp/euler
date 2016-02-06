ce2a3ce85c67c92c3aef70d5cc4f69b176c4ce37
Paul Madden <maddenp@colorado.edu>
2016-02-06 10:29:13 -0700

    Refactoring in 048

--- p/js/048.js
+++ p/js/048.js
@@ -4,15 +4,14 @@
 
 var pm = require('./pm');
 
-for (var n = 1, sum = [0]; n <= 1000; n++) {
-  var even = (n & 1) === 0 ? true : false;
+for (var n = 1, sum = [0], even = false; n <= 1000; n++) {
   var product = even ? [1] : [n];
   var limit = even ? n / 2 : (n - 1) / 2;
-  var multiplier = n * n;
   for (var i = 0; i < limit; i++) {
-    product = pm.array_trunc(pm.array_mult(product, multiplier), 10);
+    product = pm.array_trunc(pm.array_mult(product, n * n), 10);
   }
   sum = pm.array_add(sum, product);
+  even = !even;
 }
 
 console.log(pm.a2n(pm.array_trunc(sum, 10)));
