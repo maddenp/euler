328968949d2968bb254a65941a74e5393f241fdb
Paul Madden <maddenp@colorado.edu>
2016-02-06 10:25:02 -0700

    1.25x speedup in 048

--- p/js/048.js
+++ p/js/048.js
@@ -5,9 +5,12 @@
 var pm = require('./pm');
 
 for (var n = 1, sum = [0]; n <= 1000; n++) {
-  var product = [1];
-  for (var i = 0; i < n; i++) {
-    product = pm.array_trunc(pm.array_mult(product, n), 10);
+  var even = (n & 1) === 0 ? true : false;
+  var product = even ? [1] : [n];
+  var limit = even ? n / 2 : (n - 1) / 2;
+  var multiplier = n * n;
+  for (var i = 0; i < limit; i++) {
+    product = pm.array_trunc(pm.array_mult(product, multiplier), 10);
   }
   sum = pm.array_add(sum, product);
 }
