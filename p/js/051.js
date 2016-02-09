c4f6b325e8929945136e6fa709cc6dd898752b52
Paul Madden <maddenp@colorado.edu>
2016-02-08 21:18:25 -0700

    051: more variable-name changes

--- p/js/051.js
+++ p/js/051.js
@@ -10,8 +10,8 @@ var primes_and_is_prime = pm.primes(1, limit, true);
 var primes = primes_and_is_prime[0];
 var is_prime = primes_and_is_prime[1];
 
-out: for (var primes_idx = 0; primes_idx < primes.length; primes_idx++) {
-  var prime = pm.n2a(primes[primes_idx]);
+prime_loop: for (var primenum = 0; primenum < primes.length; primenum++) {
+  var prime = pm.n2a(primes[primenum]);
   var mask = [];
   for (var zeroes = 0; zeroes < prime.length; zeroes++) {
     mask.push(0);
@@ -20,19 +20,21 @@ out: for (var primes_idx = 0; primes_idx < primes.length; primes_idx++) {
     pm.array_inc(mask, 2);
     var tweaked = prime.slice();
     var generated_primes = [];
-    for (var replacement = 0; replacement < 10; replacement++) {
+    digit_loop: for (var digit = 0; digit < 10; digit++) {
       for (var position = 0; position < prime.length; position++) {
         if (mask[position]) {
-          tweaked[position] = replacement;
+          tweaked[position] = digit;
         }
       }
-      if (tweaked[0] === 0) continue;
+      if (tweaked[0] === 0) {
+        continue;
+      }
       var candidate = pm.a2n(tweaked);
       if (is_prime[candidate]) {
         generated_primes.push(candidate);
         if (generated_primes.length === goal) {
           console.log(pm.array_min(generated_primes));
-          break out;
+          break prime_loop;
         }
       }
     }
