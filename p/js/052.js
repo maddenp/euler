ce15941db94373cf4c307147cf0b9e954f2dcb51
Paul Madden <maddenp@colorado.edu>
2016-02-17 12:14:07 -0700

    13% speedup in 052 vs baseline

--- p/js/052.js
+++ p/js/052.js
@@ -4,9 +4,8 @@
 
 var pm = require('./pm');
 
-var n = 0;
-
-function is_permutation(a1, n) {
+function is_permutation(a, n) {
+  var a1 = a.slice().sort();
   var a2 = pm.n2a(n).sort();
   if (a1.length !== a2.length) return false;
   for (var i = 0; i < a1.length; i++) {
@@ -15,10 +14,13 @@ function is_permutation(a1, n) {
   return true;
 }
 
-n: while (++n) {
-  var a = pm.n2a(n).sort();
+var a = [0];
+var n = 0;
+
+loop: while (++n) {
+  pm.array_inc(a);
   for (var m = 2; m <= 6; m++) {
-    if (!is_permutation(a, n * m)) continue n;
+    if (!is_permutation(a, n * m)) continue loop;
   }
   console.log(n);
   break;
