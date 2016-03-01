358827e32d8d0844687890eefabfed79c2a60791
Paul Madden <maddenp@colorado.edu>
2016-03-01 07:18:09 -0700

    Forgot to add 053.js...

---
+++ p/js/053.js
@@ -0,0 +1,23 @@
+/* jshint node: true */
+
+"use strict";
+
+var pm = require('./pm');
+
+var choose = (function (n, r) {
+  var memo = {};
+  return function (n, r) {
+    var f = function (x) { return memo[x] || (memo[x] = pm.factorial(x)); };
+    return f(n) / (f(r) * f(n - r));
+  };
+})();
+
+var count = 0;
+
+for (var n = 23; n <= 100; n++) {
+  for (var r = 1; r <= n; r++) {
+    if (Math.floor(choose(n, r)) > 1000000) ++count;
+  }
+}
+
+console.log(count);
