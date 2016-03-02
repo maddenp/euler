75dde9d241ffba14d5900e3e00fcd6e06dc56e9b
Paul Madden <maddenp@colorado.edu>
2016-03-02 15:53:17 -0700

    Add p/js/055.js & update tests

---
+++ p/js/055.js
@@ -0,0 +1,20 @@
+/* jshint node: true */
+
+"use strict";
+
+var pm = require('./pm');
+
+var count = 0;
+
+n: for (var n = 1; n < 10000; n++) {
+  var a1 = pm.n2a(n);
+  for (var i = 0; i <= 50; i++) {
+    var a2 = pm.array_reverse(a1);
+    var a3 = pm.array_add(a1, a2);
+    if (pm.is_palindrome(a3)) continue n;
+    a1 = a3;
+  }
+  ++count;
+}
+
+console.log(count);
