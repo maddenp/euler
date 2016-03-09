1f3fa7844e2938c3c4c074d77487f93444afd3ca
Paul Madden <maddenp@colorado.edu>
2016-03-09 12:04:37 -0700

    057 correct but slow + update tests

--- p/js/057.js
+++ p/js/057.js
@@ -3,29 +3,27 @@
 
 "use strict";
 
-// 1 + 1/2 = 3/2 = 1.5
-// 1 + 1/(2 + 1/2) = 7/5 = 1.4
-// 1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
-// 1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
-
 const pm = require('./pm');
 
 const f_rec = (limit, depth) => {
   if (depth === limit) {
-    return {num: 2, den: 1};
+    return {num: [2], den: [1]};
   } else {
     const x = f_rec(limit, depth + 1);
-    return {num: 2 * x.num + x.den, den: 1 * x.num};
+    return {num: pm.array_add(pm.array_mult(x.num, 2), x.den), den: x.num};
   }
 };
 
 const f = limit => {
   const x = f_rec(limit, 1);
-  return {num: x.num + x.den, den: x.num};
+  return {num: pm.array_add(x.num, x.den), den: x.num};
 };
 
-for (var i = 1; i <= 100; i++) {
+var count = 0;
+
+for (var i = 1; i <= 1000; i++) {
   var x = f(i);
-  var yep = pm.ndigits(x.num) > pm.ndigits(x.den) ? '###' : '';
-  console.log(yep, x);
+  if (x.num.length > x.den.length) ++count;
 }
+
+console.log(count);
