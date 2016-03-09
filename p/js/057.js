5c8a0c95a0fdf582d497460a135e0ae50c7911c0
Paul Madden <maddenp@colorado.edu>
2016-03-09 12:07:09 -0700

    057: embed f_rec() in f()

--- p/js/057.js
+++ p/js/057.js
@@ -5,24 +5,23 @@
 
 const pm = require('./pm');
 
-const f_rec = (limit, depth) => {
-  if (depth === limit) {
-    return {num: [2], den: [1]};
-  } else {
-    const x = f_rec(limit, depth + 1);
-    return {num: pm.array_add(pm.array_mult(x.num, 2), x.den), den: x.num};
-  }
-};
-
 const f = limit => {
-  const x = f_rec(limit, 1);
+  const f_rec = (limit, depth) => {
+    if (depth === limit) {
+      return {num: [2], den: [1]};
+    } else {
+      let x = f_rec(limit, depth + 1);
+      return {num: pm.array_add(pm.array_mult(x.num, 2), x.den), den: x.num};
+    }
+  };
+  let x = f_rec(limit, 1);
   return {num: pm.array_add(x.num, x.den), den: x.num};
 };
 
 var count = 0;
 
 for (var i = 1; i <= 1000; i++) {
-  var x = f(i);
+  let x = f(i);
   if (x.num.length > x.den.length) ++count;
 }
 
