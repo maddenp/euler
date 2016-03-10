803a696c59f0ca983d0c6520e60c6d845d9febc9
Paul Madden <maddenp@colorado.edu>
2016-03-10 07:22:12 -0700

    27x speedup in 057 using closed-form calculations for num, den sequences

--- p/js/057.js
+++ p/js/057.js
@@ -5,24 +5,24 @@
 
 const pm = require('./pm');
 
-const f = limit => {
-  const f_rec = (limit, depth) => {
-    if (depth === limit) {
-      return {num: [2], den: [1]};
-    } else {
-      let x = f_rec(limit, depth + 1);
-      return {num: pm.array_add(pm.array_mult(x.num, 2), x.den), den: x.num};
-    }
-  };
-  let x = f_rec(limit, 1);
-  return {num: pm.array_add(x.num, x.den), den: x.num};
-};
+var a1 = [3], a2 = [7];
+var b1 = [2], b2 = [5];
 
 var count = 0;
 
-for (var i = 1; i <= 1000; i++) {
-  let x = f(i);
-  if (x.num.length > x.den.length) ++count;
+for (var i = 0; i < 998; i++) {
+  var ax = pm.array_add(pm.array_mult(a2, 2), a1);
+  var bx = pm.array_add(pm.array_mult(b2, 2), b1);
+  a1 = a2; a2 = ax;
+  b1 = b2; b2 = bx;
+  if (ax.length > bx.length) ++count;
 }
 
 console.log(count);
+
+/* Notes
+ *
+ *   Numerator, see: https://oeis.org/A001333
+ * Denominator, see: https://oeis.org/A000129
+ *
+ */
