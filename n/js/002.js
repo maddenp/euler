2c5fb4f40fe41106ebbd9a0d24cb0d0720d20a47
bud manstrong <nbpeth@gmail.com>
2016-03-13 14:56:11 -0500

    functional?

--- n/js/002.js
+++ n/js/002.js
@@ -1,14 +1,26 @@
 function evenFibs(before, current, sum){
-	if(before > 40000000 || current > 4000000){
+	if(current > 4000000){
 		return sum;
 	}
-	else if(current % 2 == 0){
-		return evenFibs(current, before + current, sum)
-	}
-	else{
-		return evenFibs(current, before + current, sum += current)
 
-	}
+	return isEven(current)
+		? evenFibs(current, moveAlong(before,current), sum)
+		: evenFibs(current, moveAlong(before,current), sum += current)
+
+	// else if(current % 2 == 0){
+	// 	return evenFibs(current, before + current, sum)
+	// }
+	// else{
+	// 	return evenFibs(current, before + current, sum += current)
+	// }
+}
+
+function moveAlong(previous, current){
+	return previous + current
+}
+
+function isEven(number){
+	return number % 2 == 0
 }
 
 console.log(evenFibs(0, 1, 0))
