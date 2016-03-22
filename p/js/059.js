3c9f94b5e5056928ab20221c9606979d918977f2
Paul Madden <maddenp@colorado.edu>
2016-03-21 22:55:03 -0600

    Supplement 059 with English vowel frequency analysis

--- p/js/059.js
+++ p/js/059.js
@@ -5,27 +5,58 @@
 
 const pm = require('./pm');
 
+const lc_lo = 97;
+const lc_hi = 122
+const uc_lo = 65;
+const uc_hi = 90;
+const printable_lo = 32;
+const printable_hi = 126;
+const vowel_freq_english = {a: 0.08167, e: 0.12702, i: 0.06966, o: 0.07507, u: 0.02758};
+
+var count = [];
+var min_error = Number.MAX_SAFE_INTEGER;
+var min_plaintext = undefined;
+
 var ciphertext = pm.readfile('059.dat');
 ciphertext = ciphertext.split(',');
 ciphertext = ciphertext.slice(0, ciphertext.length);
 ciphertext = ciphertext.map(Number);
 
-var plaintext = [];
-
-for (var a1 = 97; a1 <= 122; a1++) {
-  for (var a2 = 97; a2 <= 122; a2++) {
-    keys: for (var a3 = 97; a3 <= 122; a3++) {
+for (var a1 = lc_lo; a1 <= lc_hi; a1++) {
+  for (var a2 = lc_lo; a2 <= lc_hi; a2++) {
+    keys: for (var a3 = lc_lo; a3 <= lc_hi; a3++) {
       var key = [a1, a2, a3];
+      var sum = 0;
+      for (var i = printable_lo; i <= printable_hi; i++) {
+        count[i] = 0;
+      }
+      var plaintext = [];
       for (var i = 0; i < ciphertext.length; i++) {
         var c = ciphertext[i] ^ key[i % key.length];
-        if (c < 32 || c > 126) continue keys;
+        if (c < printable_lo || c > printable_hi) continue keys;
         plaintext[i] = c;
+        if ((c >= uc_lo && c <= uc_hi) || (c >= lc_lo && c <= lc_hi)) {
+          ++sum;
+          ++count[c];
+        }
       }
-//       console.log('--- ' + key);
-//       console.log(plaintext.reduce((m, e) => m + String.fromCharCode(e), ''));
-      if (a1 === 103 && a2 === 111 && a3 === 100) {
-        console.log(pm.array_sum(plaintext));
+      var p = count.map(n => n / sum);
+      var vowel_freq = {
+        a: p[65] + p[97],
+        e: p[69] + p[101],
+        i: p[73] + p[105],
+        o: p[79] + p [111],
+        u: p[85] + p[117]
+      };
+      var error = Object.keys(vowel_freq).reduce((m, e) => (
+        m + Math.pow(vowel_freq_english[e] - vowel_freq[e], 2)
+      ), 0);
+      if (error < min_error) {
+        min_error = error;
+        min_plaintext = plaintext;
       }
     }
   }
 }
+
+console.log(pm.array_sum(min_plaintext));
