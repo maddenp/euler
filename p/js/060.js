4aff474085bb6d3dac6281f0359967112e3ff105
Paul Madden <maddenp@colorado.edu>
2016-04-16 11:40:42 -0600

    060 Chromebook time: 1m22.939s (17x speedup over last version)

--- p/js/060.js
+++ p/js/060.js
@@ -5,37 +5,39 @@
 
 const pm = require('./pm');
 
-const fail = (p1, p2) => {
+const pairable = (p1, p2) => {
   const n1 = p2 * Math.pow(10, pm.ndigits(p1)) + p1;
   const n2 = p1 * Math.pow(10, pm.ndigits(p2)) + p2;
-  return x.check(n1) && x.check(n2) ? false : true;
+  return pm.prime.check(n1) && pm.prime.check(n2) ? true : false;
 };
 
-const f = (a, p, sum, limit) => {
-  if (a.some(n => fail(n, p))) return false;
-  if (a.length + 1 === limit) {
-    if (sum < low) low = sum;
-    return true;
+const f = (p, q, pairs, sum, depth) => {
+  if (pairable(p, q)) {
+    sum += q;
+    if (depth === goal) {
+      if (sum + p < low) low = sum + p;
+    } else {
+      Object.keys(pairs).forEach(x => {
+        f(p, pairs[x].val, pairs[x].sub, sum, depth + 1);
+      });
+      pairs[p] = {val: p, sub: {}};
+    }
   }
-  for (var i = 0, q = 0; q !== p; q = x.at(i++)) {}
-  while (true) {
-    q = x.at(i++);
-    var newsum = sum + q;
-    if (newsum > low) break;
-    if (f(a.concat(p), q, newsum, limit)) break;
-  }
-  return false;
 };
 
-const x = pm.prime;
+const goal = 5;
 
+var i = 0;
 var low = Number.MAX_SAFE_INTEGER;
-
-var i = 1;
+var roots = {};
 
 while (true) {
-  var p = x.at(i++);
-  f([], p, p, 5);
+  var p = pm.prime.at(i++);
+  if (p === 2 || p === 5) continue;
+  Object.keys(roots).forEach(q => {
+    f(p, roots[q].val, roots[q].sub, 0, 2);
+  });
+  roots[p] = {val: p, sub: {}};
   if (p > low) break;
 }
 
