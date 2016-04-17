891de04e4a17290ebd2e8576cfe8cbf183e0fb7f
Paul Madden <maddenp@colorado.edu>
2016-04-17 16:46:21 -0600

    I don't think the previous 060 technique was sound (this one is, but is slower)

--- p/js/060.js
+++ p/js/060.js
@@ -19,23 +19,29 @@ const search = (p, q, roots, sum, depth) => {
   q = parseInt(q);
   if (pairable(p, q)) {
     sum += q;
-    if (depth === goal) {
-      console.log(sum + p);
-      process.exit();
-    } else {
-      pairs.a.forEach(r => search(p, r, pairs, sum, depth + 1));
-      pairs.a.push(p);
-      pairs.o[p] = node();
+    if (sum < low) {
+      if (depth === goal && sum + p < low) {
+        low = sum + p;
+      } else {
+        pairs.a.forEach(r => search(p, r, pairs, sum, depth + 1));
+        pairs.a.push(p);
+        pairs.o[p] = node();
+      }
     }
   }
 };
 
 var i = 0;
+var low = Number.MAX_SAFE_INTEGER;
 var roots = node();
 
 while (true) {
   var p = pm.prime.at(i++);
+  if (p === 2 || p === 5) continue;
   roots.a.forEach(q => search(p, q, roots, 0, 2));
   roots.a.push(p);
   roots.o[p] = node();
+  if (p > low) break;
 }
+
+console.log(low);
