938f10d733e9125bd6132fabfc72c1b32152441e
Paul Madden <maddenp@colorado.edu>
2016-04-20 22:24:20 -0600

    Minor reworking of clique sum

--- p/js/060.js
+++ p/js/060.js
@@ -13,14 +13,16 @@ const pairable = (p1, p2) => (
   pm.prime.check(cat(p1, p2)) && pm.prime.check(cat(p2, p1)) ? true : false
 );
 
-const max_clique_with = (p, roots) => {
+const sum_of_clique_with = (p, roots) => {
   var clique = [p];
+  var sum = parseInt(p);
   Object.keys(roots).forEach(q => {
-    if (q !== p && clique.every(r => roots[r][q])) clique.push(q);
+    if (q !== p && clique.every(r => roots[r][q])) {
+      clique.push(q);
+      sum += parseInt(q);
+    }
   });
-  if (clique.length === goal) {
-    return pm.array_sum(clique.map(n => parseInt(n)));
-  }
+  return clique.length === goal ? sum : false;
 };
 
 var chunksize = 1000;
@@ -48,7 +50,7 @@ while (p < low) {
   }
 
   Object.keys(roots).forEach(p => {
-    var sum = max_clique_with(p, roots);
+    var sum = sum_of_clique_with(p, roots);
     if (sum && sum < low) low = sum;
   });
 
