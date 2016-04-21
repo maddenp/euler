ca160ba567654f159532216a4f4703d653f6796f
Paul Madden <maddenp@colorado.edu>
2016-04-20 20:09:38 -0600

    Saving failed experiment avoiding int <-> string conversions (slower)

--- p/js/060.js
+++ p/js/060.js
@@ -5,7 +5,7 @@
 
 const pm = require('./pm');
 
-const cat = (a, b) => parseInt(a + b);
+const cat = (a, b) => b * Math.pow(10, pm.ndigits(a)) + a;
 
 const goal = 5;
 
@@ -13,10 +13,12 @@ const pairable = (p1, p2) => (
   pm.prime.check(cat(p1, p2)) && pm.prime.check(cat(p2, p1)) ? true : false
 );
 
-const max_clique_with = (p, roots) => {
-  var clique = [p];
-  Object.keys(roots).forEach(q => {
-    if (q !== p && clique.every(r => roots[r][q])) clique.push(q);
+const max_clique_with = (r1, roots) => {
+  var clique = [r1];
+  var p = roots[r1].val;
+  Object.keys(roots).forEach(r2 => {
+    var q = roots[r2].val;
+    if (q !== p && clique.every(r3 => roots[r3].pairables[r2])) clique.push(r2);
   });
   if (clique.length === goal) {
     return pm.array_sum(clique.map(n => parseInt(n)));
@@ -36,20 +38,22 @@ while (p < low) {
     var p = pm.prime.at(primeidx++);
     if (p === 2 || p === 5) continue;
     if (p > low) break;
-    roots[p] = {};
-    Object.keys(roots).forEach(q => {
-      if (q !== p) {
+    var s = p.toString();
+    roots[s] = {val: p, pairables: {}};
+    Object.keys(roots).forEach(r => {
+      var q = roots[r].val;
+      if (q !== s) {
         if (pairable(p, q)) {
-          roots[p][q] = true;
-          roots[q][p] = true;
+          roots[s].pairables[r] = q;
+          roots[r].pairables[s] = p;
         }
       }
     });
   }
 
-  Object.keys(roots).forEach(p => {
-    var sum = max_clique_with(p, roots);
-    if (sum && sum < low) low = sum;
+  Object.keys(roots).forEach(r => {
+    var n = max_clique_with(r, roots);
+    if (n && n < low) low = n;
   });
 
   limit = low === Number.MAX_SAFE_INTEGER ? limit + chunksize : low;
