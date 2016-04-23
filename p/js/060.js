7fed42723a1a79cc7ddb19d1db74b406159b05fa
Paul Madden <maddenp@colorado.edu>
2016-04-22 18:47:50 -0600

    Break-out conditions => < 1 minute Chromebook run time

--- p/js/060.js
+++ p/js/060.js
@@ -37,23 +37,39 @@ while (p < low) {
 
   const k = Object.keys(roots);
   for (var i1 = 0; i1 < k.length; i1++) {
+//     var sum = 0;
     var a = k[i1];
-    if (parseInt(a) > low) continue;
+    var ia = parseInt(a);
+//     sum += ia;
+//     if (sum * 5 > low) break;
+    if (ia * 5 > low) break;
     for (var i2 = i1 + 1; i2 < k.length; i2++) {
       var b = k[i2];
-      if (parseInt(b) > low) continue;
+      var ib = parseInt(b);
+//       sum += ib;
+//       if (sum + ib * 3 > low) break;
+      if (ia + ib * 4 > low) break;
       if (!roots[a][b]) continue;
       for (var i3 = i2 + 1; i3 < k.length; i3++) {
         var c = k[i3];
-        if (parseInt(c) > low) continue;
+        var ic = parseInt(c);
+//         sum += ic
+//         if (sum + ic * 2 > low) break;
+        if (ia + ib + ic * 3 > low) break;
         if (!roots[a][c] || !roots[b][c]) continue;
         for (var i4 = i3 + 1; i4 < k.length; i4++) {
           var d = k[i4];
-          if (parseInt(d) > low) continue;
+          var id = parseInt(d);
+//           sum += id;
+//           if (sum + id > low) break;
+          if (ia + ib + ic + id * 2 > low) break;
           if (!roots[a][d] || !roots[b][d] || !roots[c][d]) continue;
           for (var i5 = i4 + 1; i5 < k.length; i5++) {
             var e = k[i5];
-            if (parseInt(e) > low) continue;
+            var ie = parseInt(e);
+//             sum += ie;
+//             if (sum > low) break;
+            if (ia + ib + ic + id + ie > low) break;
             if (!roots[a][e] || !roots[b][e] || !roots[c][e] || !roots[d][e]) continue;
             var sum = pm.array_sum([a, b, c, d, e].map(n => parseInt(n)));
             if (sum < low) low = sum;
