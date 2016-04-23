a927072b46d2913ef8504f15bdaa4159ebbaa4d9
Paul Madden <maddenp@colorado.edu>
2016-04-22 21:14:07 -0600

    Generate pairs on demand

--- p/js/060.js
+++ p/js/060.js
@@ -14,54 +14,45 @@ const pairable = (p1, p2) => (
 var limit = 10000;
 var low = Number.MAX_SAFE_INTEGER;
 var max = 0;
-var primeidx = 0;
 var roots = {};
 
-const make_pairs_for_prime_at_idx = idx => {
+const f = idx => {
   const p = pm.prime.at(idx);
-  roots[p] = {};
-  Object.keys(roots).forEach(q => {
-    if (pairable(p, q)) {
-      roots[p][q] = true;
-      roots[q][p] = true;
-    }
-  });
+  if (!roots[p]) {
+    roots[p] = {};
+    Object.keys(roots).forEach(q => {
+      if (pairable(p, q)) {
+        roots[p][q] = true;
+        roots[q][p] = true;
+      }
+    });
+  }
   return p;
 };
 
-while (make_pairs_for_prime_at_idx(primeidx++) < limit);
-
-const k = Object.keys(roots);
-
-for (var i1 = 0; i1 < k.length; i1++) {
-  var a = k[i1];
-  var ia = parseInt(a);
-  if (ia > max) max = ia;
-  var sum_a = ia;
+for (var i1 = 1, a = 0; a < limit; i1++) {
+  a = f(i1);
+  if (a > max) max = a;
+  var sum_a = a;
   if (sum_a * 5 > low) break;
-  for (var i2 = i1 + 1; i2 < k.length; i2++) {
-    var b = k[i2];
-    var ib = parseInt(b);
-    var sum_b = sum_a + ib;
-    if (sum_b + ib * 3 > low) break;
+  for (var i2 = i1 + 1, b = 0; b < limit; i2++) {
+    b = f(i2);
+    var sum_b = sum_a + b;
+    if (sum_b + b * 3 > low) break;
     if (!roots[a][b]) continue;
-    for (var i3 = i2 + 1; i3 < k.length; i3++) {
-      var c = k[i3];
-      var ic = parseInt(c);
-      var sum_c = sum_b + ic;
-      if (sum_c + ic * 2 > low) break;
+    for (var i3 = i2 + 1, c = 0; c < limit; i3++) {
+      c = f(i3);
+      var sum_c = sum_b + c;
+      if (sum_c + c * 2 > low) break;
       if (!roots[a][c] || !roots[b][c]) continue;
-      for (var i4 = i3 + 1; i4 < k.length; i4++) {
-        var d = k[i4];
-        var id = parseInt(d);
-        var sum_d = sum_c + id;
-        if (sum_d + id > low) break;
-        if (ia + ib + ic + id * 2 > low) break;
+      for (var i4 = i3 + 1, d = 0; d < limit; i4++) {
+        d = f(i4);
+        var sum_d = sum_c + d;
+        if (sum_d + d > low) break;
         if (!roots[a][d] || !roots[b][d] || !roots[c][d]) continue;
-        for (var i5 = i4 + 1; i5 < k.length; i5++) {
-          var e = k[i5];
-          var ie = parseInt(e);
-          var sum_e = sum_d + ie;
+        for (var i5 = i4 + 1, e = 0; e < limit; i5++) {
+          e = f(i5);
+          var sum_e = sum_d + e;
           if (sum_e > low) break;
           if (!roots[a][e] || !roots[b][e] || !roots[c][e] || !roots[d][e]) continue;
           if (sum_e < low) low = sum_e;
