89b886b3d65071a0864da3ee62e40493be5bad25
Paul Madden <maddenp@colorado.edu>
2016-04-22 19:35:45 -0600

    Work on optimizing 060

--- p/js/060.js
+++ p/js/060.js
@@ -11,70 +11,71 @@ const pairable = (p1, p2) => (
   pm.prime.check(cat(p1, p2)) && pm.prime.check(cat(p2, p1)) ? true : false
 );
 
-var chunksize = 10000;
-var limit = chunksize;
+var limit = 10000;
 var low = Number.MAX_SAFE_INTEGER;
+var max = 0;
 var p = 0;
 var primeidx = 0;
 var roots = {};
 
-while (p < low) {
-
-  while (p < limit) {
-    var p = pm.prime.at(primeidx++);
-    if (p === 2 || p === 5) continue;
-    if (p > low) break;
-    roots[p] = {};
-    Object.keys(roots).forEach(q => {
-      if (q !== p) {
-        if (pairable(p, q)) {
-          roots[p][q] = true;
-          roots[q][p] = true;
-        }
+while (p < limit) {
+  var p = pm.prime.at(primeidx++);
+  if (p === 2 || p === 5) continue;
+  roots[p] = {};
+  Object.keys(roots).forEach(q => {
+    if (q !== p) {
+      if (pairable(p, q)) {
+        roots[p][q] = true;
+        roots[q][p] = true;
       }
-    });
-  }
+    }
+  });
+}
 
-  const k = Object.keys(roots);
-  for (var i1 = 0; i1 < k.length; i1++) {
-    var a = k[i1];
-    var ia = parseInt(a);
-    var sum_a = ia;
-    if (sum_a * 5 > low) break;
-    for (var i2 = i1 + 1; i2 < k.length; i2++) {
-      var b = k[i2];
-      var ib = parseInt(b);
-      var sum_b = sum_a + ib;
-      if (sum_b + ib * 3 > low) break;
-      if (!roots[a][b]) continue;
-      for (var i3 = i2 + 1; i3 < k.length; i3++) {
-        var c = k[i3];
-        var ic = parseInt(c);
-        var sum_c = sum_b + ic;
-        if (sum_c + ic * 2 > low) break;
-        if (!roots[a][c] || !roots[b][c]) continue;
-        for (var i4 = i3 + 1; i4 < k.length; i4++) {
-          var d = k[i4];
-          var id = parseInt(d);
-          var sum_d = sum_c + id;
-          if (sum_d + id > low) break;
-          if (ia + ib + ic + id * 2 > low) break;
-          if (!roots[a][d] || !roots[b][d] || !roots[c][d]) continue;
-          for (var i5 = i4 + 1; i5 < k.length; i5++) {
-            var e = k[i5];
-            var ie = parseInt(e);
-            var sum_e = sum_d + ie;
-            if (sum_e > low) break;
-            if (!roots[a][e] || !roots[b][e] || !roots[c][e] || !roots[d][e]) continue;
-            if (sum_e < low) low = sum_e;
-          }
+const k = Object.keys(roots);
+
+for (var i1 = 0; i1 < k.length; i1++) {
+  var a = k[i1];
+  var ia = parseInt(a);
+  if (ia > max) max = ia;
+  var sum_a = ia;
+  if (sum_a * 5 > low) break;
+  for (var i2 = i1 + 1; i2 < k.length; i2++) {
+    var b = k[i2];
+    var ib = parseInt(b);
+    var sum_b = sum_a + ib;
+    if (sum_b + ib * 3 > low) break;
+    if (!roots[a][b]) continue;
+    for (var i3 = i2 + 1; i3 < k.length; i3++) {
+      var c = k[i3];
+      var ic = parseInt(c);
+      var sum_c = sum_b + ic;
+      if (sum_c + ic * 2 > low) break;
+      if (!roots[a][c] || !roots[b][c]) continue;
+      for (var i4 = i3 + 1; i4 < k.length; i4++) {
+        var d = k[i4];
+        var id = parseInt(d);
+        var sum_d = sum_c + id;
+        if (sum_d + id > low) break;
+        if (ia + ib + ic + id * 2 > low) break;
+        if (!roots[a][d] || !roots[b][d] || !roots[c][d]) continue;
+        for (var i5 = i4 + 1; i5 < k.length; i5++) {
+          var e = k[i5];
+          var ie = parseInt(e);
+          var sum_e = sum_d + ie;
+          if (sum_e > low) break;
+          if (!roots[a][e] || !roots[b][e] || !roots[c][e] || !roots[d][e]) continue;
+          if (sum_e < low) low = sum_e;
         }
       }
     }
   }
-
-  limit = low === Number.MAX_SAFE_INTEGER ? limit + chunksize : low;
-
 }
 
-console.log(low);
+if (low === Number.MAX_SAFE_INTEGER) {
+  console.log('No solution found.');
+} else if (max * 5 < low) {
+  console.log('Search space too small.');
+} else {
+  console.log(low);
+}
