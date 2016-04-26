cb1cf7086abfcaad14ef7500c73b79371e08f2bd
Paul Madden <maddenp@colorado.edu>
2016-04-25 19:34:54 -0600

    Use Bron-Kerbosch in 060 => 0m2.968s Chromebook time

--- p/js/060.js
+++ p/js/060.js
@@ -5,59 +5,68 @@
 
 const pm = require('./pm');
 
-const cat = (a, b) => parseInt(a + b);
+const bron_kerbosch = (pairs, r, p, x) => {
+  if (p.length === 0 && x.length === 0 && r.length === goal) {
+    var sum = pm.array_sum(r);
+    if (sum < min) min = sum;
+  } else {
+    p = p.slice();
+    x = x.slice();
+    p.slice().forEach(v => {
+      bron_kerbosch(pairs, r.concat(v), intersection(p, pairs[v]), intersection(x, pairs[v]));
+      p.splice(p.indexOf(v), 1);
+      x.push(v);
+    });
+  }
+};
 
-const pairable = (p1, p2) => (
-  pm.prime.check(cat(p1, p2)) && pm.prime.check(cat(p2, p1)) ? true : false
-);
+const intersection = (a1, a2) => {
+  const a3 = [];
+  const x1 = a1.length < a2.length ? a1 : a2;
+  const x2 = a1.length < a2.length ? a2 : a1;
+  var from = 0;
+  for (var i = 0; i < x1.length; i++) {
+    var found_at = x2.indexOf(x1[i], from);
+    if (found_at !== -1) {
+      a3.push(x1[i]);
+      from = found_at;
+    }
+  }
+  return a3;
+};
 
-const f = idx => {
-  const p = pm.prime.at(idx);
-  if (!roots[p]) {
-    roots[p] = {};
-    Object.keys(roots).forEach(q => {
+const make_pairs = (p, pairs) => {
+  if (!pairs[p]) {
+    pairs[p] = [];
+    Object.keys(pairs).forEach(q => {
       if (pairable(p, q)) {
-        roots[p][q] = true;
-        roots[q][p] = true;
+        pairs[p].push(q);
+        pairs[q].push(p);
       }
     });
   }
-  return p;
 };
 
-var limit = 9000;
-var low = Number.MAX_SAFE_INTEGER;
-var roots = {};
+const pairable = (p1, p2) => (
+  pm.miller_rabin(parseInt(p1 + p2)) && pm.miller_rabin(parseInt(p2 + p1))
+);
 
-for (var i1 = 1, a = 0; a < limit; i1++) {
-  a = f(i1);
-  var sum_a = a;
-  if (sum_a * 5 > low) break;
-  for (var i2 = i1 + 1, b = 0; b < limit; i2++) {
-    b = f(i2);
-    var sum_b = sum_a + b;
-    if (sum_b + b * 3 > low) break;
-    if (!roots[a][b]) continue;
-    for (var i3 = i2 + 1, c = 0; c < limit; i3++) {
-      c = f(i3);
-      var sum_c = sum_b + c;
-      if (sum_c + c * 2 > low) break;
-      if (!roots[a][c] || !roots[b][c]) continue;
-      for (var i4 = i3 + 1, d = 0; d < limit; i4++) {
-        d = f(i4);
-        var sum_d = sum_c + d;
-        if (sum_d + d > low) break;
-        if (!roots[a][d] || !roots[b][d] || !roots[c][d]) continue;
-        for (var i5 = i4 + 1, e = 0; e < limit; i5++) {
-          e = f(i5);
-          var sum_e = sum_d + e;
-          if (sum_e > low) break;
-          if (!roots[a][e] || !roots[b][e] || !roots[c][e] || !roots[d][e]) continue;
-          if (sum_e < low) low = sum_e;
-        }
-      }
-    }
-  }
+const goal = 5;
+const limit = 1051; // experimentally derived
+const pairs = {};
+
+for (var i = 0; i < limit; i++) {
+  make_pairs(pm.prime.at(i), pairs);
 }
 
-if (low !== Number.MAX_SAFE_INTEGER) console.log(low);
+var min = Number.MAX_SAFE_INTEGER;
+
+bron_kerbosch(pairs, [], Object.keys(pairs).map(s => parseInt(s)), []);
+
+if (min === Number.MAX_SAFE_INTEGER) {
+  console.log('No ' + goal + '-clique found');
+} else if (pm.prime.primes[pm.prime.primes.length - 1] * goal < min) {
+  console.log('Insufficient search space');
+} else {
+  console.log(min);
+}
