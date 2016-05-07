1099bd65ae944ef4317f9777d199b53d27b13fe1
Paul Madden <maddenp@colorado.edu>
2016-05-07 14:53:15 -0600

    064 lists correct periods for first 13 non-squares

--- p/js/064.js
+++ p/js/064.js
@@ -5,22 +5,32 @@
 
 const pm = require('./pm');
 
-const f = (o) => {
-  o.b = (o.n - o.c * o.c) / o.b;
-  o.a = Math.floor((Math.sqrt(o.n) - o.c) / o.b);
-  o.c = -o.c - o.a * o.b;
-  return o;
-}
+const g = n => {
+  
+  const f = (o) => {
+    o.b = (o.n - o.c * o.c) / o.b;
+    o.a = Math.floor((Math.sqrt(o.n) - o.c) / o.b);
+    o.c = -o.c - o.a * o.b;
+  };
 
-const n = 23;
-const a = Math.floor(Math.sqrt(n));
-const b = 1;
-const c = -a;
+  const a = Math.floor(Math.sqrt(n));
+  const b = 1;
+  const c = -a;
+  const seen = {b: {c: true}};
 
-var o = {n, a, b, c};
-console.log(o);
+  var o = {n, a, b, c};
 
-for (var i = 0; i < 10; i++) {
-  o = f(o);
-  console.log(o);
+  for (var i = 0; i < 10; i++) {
+    f(o);
+    if (seen[o.b] && seen[o.b][o.c]) return i;
+    seen[o.b] = seen[o.b] || {};
+    seen[o.b][o.c] = true;
+  }
+  console.log(seen);
+};
+
+for (var n = 2; n <= 13; n++) {
+  if (Math.sqrt(n) !== Math.floor(Math.sqrt(n))) {
+    console.log(n, g(n));
+  }
 }
