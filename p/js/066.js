3b4875dff73738e0db60a23d9c927a234da0f220
Paul Madden <maddenp@colorado.edu>
2016-05-30 12:04:12 -0600

    Clean-up on 066 before more work

--- p/js/066.js
+++ p/js/066.js
@@ -5,18 +5,17 @@
 
 const apply_to_convergents = (n, f) => {
   
+  // http://goo.gl/uSYJLz re: method for calculating convergent
+
   var a = Math.floor(Math.sqrt(n));
   var b = 1;
   var c = -a;
-  var i = 0;
 
   var conv_num_old = 1;
   var conv_den_old = 0;
 
   var conv_num = a;
   var conv_den = 1;
-
-  // http://goo.gl/uSYJLz for info on method for calculating convergent
 
   while (true) {
     b = (n - c * c) / b;
@@ -35,13 +34,9 @@ const apply_to_convergents = (n, f) => {
 
 };
 
-const f = (d, x, y) => {
-  console.log('x='+x+' d='+d+' y='+y);
-  return x * x - d * y * y === 1;
-}
+const f = (d, x, y) => x * x - d * y * y === 1;
 
-for (var i = 2; i <= 1000; i++) {
-  console.log('###',i);
-  if (Math.sqrt(i) === Math.floor(Math.sqrt(i))) continue;
-  console.log(apply_to_convergents(i, f));
+for (var d = 2; d <= 7; d++) {
+  if (Math.sqrt(d) === Math.floor(Math.sqrt(d))) continue;
+  console.log(apply_to_convergents(d, f));
 }
