8bdb0bb186c27a63a501390974c07851b9c491c7
Paul Madden <maddenp@colorado.edu>
2016-07-26 07:52:04 -0600

    073: Optimization, Chromebook time 1.9s

--- p/js/073.js
+++ p/js/073.js
@@ -5,6 +5,8 @@
 
 const n = 12000;
 
+var count = 0;
+
 // Algorithm as given by Wikipedia's article on Farey sequences
 
 var a = 0;
@@ -12,13 +14,10 @@ var b = 1;
 var c = 1;
 var d = n;
 
-const as = [];
-const bs = [];
-
-as.push(a);
-bs.push(b);
-
 while (c <= n) {
+
+  if (a/b > 1/2 && a/b < 2/3) ++count;
+
   var k = Math.floor((n + b) / d);
   var old_a = a;
   var old_b = b;
@@ -26,15 +25,7 @@ while (c <= n) {
   b = d;
   c = k * c - old_a;
   d = k * d - old_b;
-  as.push(a);
-  bs.push(b);
-}
 
-var count = 0;
-
-for (var i = 0; i < as.length; i++) {
-  var x = as[i] / bs[i];
-  if (x > 1/2 && x < 2/3) ++count;
 }
 
 console.log(count);
