3b31ccf6c652f428a9130bd198c18849c202bfa7
Paul Madden <maddenp@colorado.edu>
2016-08-10 13:59:31 -0600

    074: Remove debug prints

--- p/js/074.js
+++ p/js/074.js
@@ -22,16 +22,12 @@ const chain_lengths = {};
 
 var sixties = 0;
 
-// var ns = [69, 169];
-// for (var x = 0; x < ns.length; x++) {
-//   var n = ns[x];
 for (var n = 1; n < 1000000; n++) {
   var count = 1;
   var m = n;
   var seen_arr = [n];
   var seen_map = {n: true};
   while (true) {
-//     console.log(n + ': adding ' + m + ' (count ' + count + ')');
     var sum = 0;
     while (m > 0) {
       sum += factorials[m % 10];
@@ -40,14 +36,12 @@ for (var n = 1; n < 1000000; n++) {
     m = sum;
     if (chain_lengths[m])
     {
-//       console.log(n + ': already know chain ' + m);
       count += chain_lengths[m];
       if (count == 60) ++sixties;
       var decrement = 1;
       for (var i = 0; i < seen_arr.length; i++) {
         var e = seen_arr[i];
         if (chain_lengths[e]) break;
-//         console.log(n + ': setting length of ' + e + ' to ' + count);
         chain_lengths[e] = count;
         if (e === m) decrement = 0;
         count -= decrement;
@@ -55,13 +49,11 @@ for (var n = 1; n < 1000000; n++) {
       break;
     }
     if (seen_map[m]) {
-//       console.log(n + ': loop at ' + m);
       if (count == 60) ++sixties;
       var decrement = 1;
       for (var i = 0; i < seen_arr.length; i++) {
         var e = seen_arr[i];
         if (chain_lengths[e]) break;
-//         console.log(n + ': setting length of ' + e + ' to ' + count);
         chain_lengths[e] = count;
         if (e === m) decrement = 0;
         count -= decrement;
@@ -72,7 +64,6 @@ for (var n = 1; n < 1000000; n++) {
     seen_map[m] = true;
     count += 1;
   }
-//   console.log(JSON.stringify(chain_lengths));
 }
 
 console.log(sixties);
