f9f48c87457521418223f555d00ed8a0fd0a80a5
Paul Madden <maddenp@colorado.edu>
2016-09-25 18:34:52 -0600

    031: Better solution from Euler's PDF

--- p/js/031.js
+++ p/js/031.js
@@ -3,31 +3,13 @@
 
 "use strict";
 
-var count = 0;
+const amount = 200;
+const coins = [1, 2, 5, 10, 20, 50, 100, 200];
 
-for (var p200 = 0; p200 <= 1; p200++) {
-  var r200 = 200 - p200 * 200;
-  for (var p100 = 0; p100 <= Math.floor(r200/100); p100++) {
-    var r100 = r200 - p100 * 100;
-    for (var p50 = 0; p50 <= Math.floor(r100/50); p50++) {
-      var r50 = r100 - p50 * 50;
-      for (var p20 = 0; p20 <= Math.floor(r50/20); p20++) {
-        var r20 = r50 - p20 * 20;
-        for (var p10 = 0; p10 <= Math.floor(r20/10); p10++) {
-          var r10 = r20 - p10 * 10;
-          for (var p5 = 0; p5 <= Math.floor(r10/5); p5++) {
-            var r5 = r10 - p5 * 5;
-            for (var p2 = 0; p2 <= Math.floor(r5/2); p2++) {
-              var r2 = r5 - p2 * 2;
-              for (var p1 = 0; p1 <= Math.floor(r2/1); p1++) {
-                if (p200 * 200 + p100 * 100 + p50 * 50 + p20 * 20 + p10 * 10 + p5 * 5 + p2 * 2 + p1 * 1 === 200) ++count;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+const cc = (a, i) => {
+  if (i <= 0) return 1;
+  for (var x = 0; a >=0; a -= coins[i]) x += cc(a, i - 1);
+  return x;
+};
 
-console.log(count);
+console.log(cc(amount, coins.length - 1));
