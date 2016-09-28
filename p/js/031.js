581bef83d72fcf8e43988de23b7defde03ca53f1
Paul Madden <maddenp@colorado.edu>
2016-09-28 18:41:16 -0400

    031: Remove assumption that 1 is lowest coin

--- p/js/031.js
+++ p/js/031.js
@@ -7,7 +7,7 @@ const amount = 200;
 const coins = [1, 2, 5, 10, 20, 50, 100, 200];
 
 const cc = (amount, i) => {
-  if (amount === 0 || coins[i] === 1) return 1;
+  if (amount === 0 || coins[i] === coins[0]) return 1;
   if (amount < coins[i]) return cc(amount, i - 1);
   return cc(amount, i - 1) + cc(amount - coins[i], i);
 }
