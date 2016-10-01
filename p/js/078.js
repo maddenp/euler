9b0502dd28025a5991764754d4705219a36b37ea
Paul Madden <maddenp@colorado.edu>
2016-10-01 09:09:23 -0600

    078: (too slow) solution + test

---
+++ p/js/078.js
@@ -0,0 +1,24 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+
+"use strict";
+
+const pm = require('./pm');
+
+var n = 1;
+
+const check = (a, zeros) => {
+  for (var i = a.length - 1; i > a.length - 1 - zeros; i--) {
+    if (a[i] !== 0) return false;
+  }
+  return true;
+}
+  
+while (true) {
+  var a = pm.partitions.p0(n);
+  if (check(a, 6)) {
+    console.log(n);
+    break;
+  }
+  n++;
+}
