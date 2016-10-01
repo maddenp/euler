a9537284920f8b5c5aaac09b2c7dcd444b7ec046
Paul Madden <maddenp@colorado.edu>
2016-10-01 13:20:56 -0600

    078: simplify

--- p/js/078.js
+++ p/js/078.js
@@ -6,23 +6,18 @@
 const pm = require('./pm');
 
 const p = [1, 1];
-const d = 1000000;
-var n = 1;
-while (true) {
-  n++;
-  var i = 0;
-  while (true) {
-    i++;
+
+for (var n = 2; true; n++) {
+  p[n] = 0;
+  for (var i = 0; true; i++) {
     var m1 = n - i * (3 * i - 1) / 2;
     var m2 = n - i * (3 * i + 1) / 2;
-    var s = 1;
-    if (i % 2 === 0) s = -1;
-    p[n] = p[n] || 0;
-    if (m1 >= 0) p[n] += s * p[m1];
-    if (m2 >= 0) p[n] += s * p[m2];
+    if (m1 >= 0) p[n] += (i % 2 === 0 ? -1 : 1) * p[m1];
+    if (m2 >= 0) p[n] += (i % 2 === 0 ? -1 : 1) * p[m2];
     if (m1 < 0 && m2 < 0) break;
   }
-  p[n] = p[n] % d;
+  p[n] = p[n] % 1000000;
   if (p[n] === 0) break;
 }
+
 console.log(n);
