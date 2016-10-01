a9d95aa1e3647d7c2cfab4eae8648c10218ffa5f
Paul Madden <maddenp@colorado.edu>
2016-09-30 19:12:26 -0600

    031: Rewrite using optimization from overview PDF

--- p/js/031.js
+++ p/js/031.js
@@ -7,4 +7,14 @@ const pm = require('./pm');
 
 const coins = [1, 2, 5, 10, 20, 50, 100, 200];
 
-console.log(pm.a2n(pm.partitions.count(200, coins, coins.length - 1)));
+const n = 200;
+const ways = [1];
+
+for (var i = 0; i < coins.length; i++) {
+  var c = coins[i];
+  for (var t = c; t <= n; t++) {
+    ways[t] = (ways[t] || 0) + ways[t - c];
+  }
+}
+
+console.log(ways[n]);
