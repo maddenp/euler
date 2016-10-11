92f8b14b8642a46ff25b1ff3ef8a51da15f8e9d4
Paul Madden <maddenp@colorado.edu>
2016-10-10 21:08:20 -0600

    079: Programmatic solution

--- p/js/079.js
+++ p/js/079.js
@@ -7,4 +7,38 @@ const pm = require('./pm');
 
 var a = pm.readfile('079.dat').split(',').map(s => s.split('').map(d => parseInt(d)));
 
-console.log(73162890);
+var follows = {};
+
+a.forEach(n => n.forEach(d => follows[d] = {}));
+
+a.forEach(n => {
+  follows[n[1]][n[0]] = true;
+  follows[n[2]][n[1]] = true;
+});
+
+var sol = Object.keys(follows);
+
+const shift = (a, i, j) => {
+  const b = [];
+  const tmp = a[i];
+  for (var k = 0; k < a.length; k++) {
+    if (k < i || k > j) {
+      b[k] = a[k];
+    } else if (k < j) {
+      b[k] = a[k + 1];
+    } else {
+      b[k] = tmp;
+    }
+  }
+  return b;
+};
+
+Object.keys(follows).forEach(d => {
+  Object.keys(follows[d]).forEach(e => {
+    var id = sol.indexOf(d);
+    var ie = sol.indexOf(e);
+    if (ie > id) sol = shift(sol, id, ie);
+  });
+});
+
+console.log(pm.a2n(sol));
