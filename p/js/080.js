da0fe08c183cb97cd83ba9d5df4a9291304bdc06
Paul Madden <maddenp@colorado.edu>
2016-10-13 13:52:59 -0600

    080: Small updates, but integer-overflow issues exist

--- p/js/080.js
+++ p/js/080.js
@@ -30,13 +30,14 @@ const sqrt = (n, decimal_places) => {
   for (var i = 0; i <= decimal_places; i++) {
     for (var x = 0; f(p, x + 1) <= c; x++);
     var y = f(p, x);
-    s.push(x);
     c = (c - y) * 100;
     p = p * 10 + x;
     r = c - y;
+    s.push(x);
   }
 
   return s;
+
 };
 
-console.log(sqrt(2, 4));
+console.log(sqrt(2, 100));
