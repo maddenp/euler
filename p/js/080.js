2f44ce1399e8edafc6b63ee63719faed0d1d1b50
Paul Madden <maddenp@colorado.edu>
2016-10-13 13:19:30 -0600

    080: Start implementing algorithm from Wikipedia

--- p/js/080.js
+++ p/js/080.js
@@ -3,8 +3,33 @@
 
 "use strict";
 
-for (var i = 1; i <= 100; i++) {
-  var x = Math.sqrt(i);
-  if (Math.floor(x) === x) continue;
-  console.log(i);
+const pm = require('./pm');
+
+// A simplified, n e Z, 0 < n < 100 version of:
+// https://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Digit-by-digit_calculation
+
+// for (var i = 1; i < 100; i++) {
+//   var x = Math.sqrt(i);
+//   if (Math.floor(x) === x) continue;
+//   console.log(i);
+// }
+
+var n = 2;
+
+var c = n;
+var p = 0;
+var r = 0;
+var s = [];
+
+const f = (p, x) => x * (20 * p + x);
+
+for (var i = 0; i < 10; i++) {
+  for (var x = 0; f(p, x + 1) <= c; x++);
+  var y = f(p, x);
+  s.push(x);
+  c = (c - y) * 100;
+  p = p * 10 + x;
+  r = c - y;
 }
+
+console.log(s);
