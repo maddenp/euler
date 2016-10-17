d157f2bdde71460c25cde8196da1a1d3b5ee91a4
Paul Madden <maddenp@colorado.edu>
2016-10-17 07:48:23 -0600

    081: Add test data

--- p/js/081.js
+++ p/js/081.js
@@ -5,4 +5,10 @@
 
 // const pm = require('./pm');
 
-var matrix = require('fs').readFileSync('081.dat', 'utf8').trim().split('\n').map(x => x.split(','));
+// const matrix = require('fs').readFileSync('081.dat', 'utf8').trim().split('\n').map(x => x.split(','));
+
+const matrix = [[131, 673, 234, 103,  18],
+                [201,  96, 342, 965, 150],
+                [630, 803, 746, 422, 111],
+                [537, 699, 497, 121, 956],
+                [805, 732, 524,  37, 331]];
