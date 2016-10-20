c4a188eb1ef80f7d613de0836841c6bf5c969aec
Paul Madden <maddenp@colorado.edu>
2016-10-20 07:37:39 -0600

    082: Add skeleton & data

---
+++ p/js/082.js
@@ -0,0 +1,14 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+
+"use strict";
+
+// const dist = require('fs').readFileSync('082.dat', 'utf8').trim().split('\n').map(x => x.split(',').map(s => parseInt(s)));
+
+const dist = [[131, 673, 234, 103,  18],
+              [201,  96, 342, 965, 150],
+              [630, 803, 746, 422, 111],
+              [537, 699, 497, 121, 956],
+              [805, 732, 524,  37, 331]];
+
+// Dijkstra time
