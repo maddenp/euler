5b2ca3e3f898036c36c663abce2e75ee6e0bba32
Paul Madden <maddenp@colorado.edu>
2016-10-20 20:51:51 -0600

    082: Make dijkstra() function

--- p/js/082.js
+++ p/js/082.js
@@ -13,50 +13,54 @@ const matrix = [[131, 673, 234, 103,  18],
                 [537, 699, 497, 121, 956],
                 [805, 732, 524,  37, 331]];
 
-const dist = [];
-const prev = [];
-const Q = [];
+const dijkstra = source => {
 
-for (var row = 0; row < matrix.length; row++) {
-  dist[row] = [];
-  prev[row] = [];
-  for (var col = 0; col < matrix[0].length; col++) {
-    dist[row][col] = MSI;
-    prev[row][col] = undefined;
-    Q.push({row: row, col: col});
-  }
-}
+  const dist = [];
+  const prev = [];
+  const Q = [];
 
-var source = {row: 1, col: 0};
-
-dist[source.row][source.col] = matrix[source.row][source.col];
-
-while (Q.length > 0) {
-  var min_d = MSI;
-  var min_i = undefined;
-  for (var i = 0; i < Q.length; i++) {
-    var d = dist[Q[i].row][Q[i].col]
-    if (d < min_d) {
-      min_d = d;
-      min_i = i;
+  for (var row = 0; row < matrix.length; row++) {
+    dist[row] = [];
+    prev[row] = [];
+    for (var col = 0; col < matrix[0].length; col++) {
+      dist[row][col] = MSI;
+      prev[row][col] = undefined;
+      Q.push({row: row, col: col});
     }
   }
-  var u = Q.splice(min_i, 1)[0];
-  var to_u = dist[u.row][u.col];
-  var neighbors = []
-  if (u.row < dist.length - 1) neighbors.push({row: u.row + 1, col: u.col});
-  if (u.col < dist[u.row].length - 1) neighbors.push({row: u.row, col: u.col + 1});
-  if (u.row > 0) neighbors.push({row: u.row - 1, col: u.col});
-  neighbors.forEach(v => {
-    var to_v_now = dist[v.row][v.col] || MSI;
-    var to_v_alt = to_u + matrix[v.row][v.col];
-    if (to_v_alt < to_v_now) {
-      dist[v.row][v.col] = to_v_alt;
-      prev[v.row][v.col] = u;
+
+  dist[source.row][source.col] = matrix[source.row][source.col];
+
+  while (Q.length > 0) {
+    var min_d = MSI;
+    var min_i = undefined;
+    for (var i = 0; i < Q.length; i++) {
+      var d = dist[Q[i].row][Q[i].col]
+      if (d < min_d) {
+        min_d = d;
+        min_i = i;
+      }
     }
-  });
+    var u = Q.splice(min_i, 1)[0];
+    var to_u = dist[u.row][u.col];
+    var neighbors = []
+    if (u.row < dist.length - 1) neighbors.push({row: u.row + 1, col: u.col});
+    if (u.col < dist[u.row].length - 1) neighbors.push({row: u.row, col: u.col + 1});
+    if (u.row > 0) neighbors.push({row: u.row - 1, col: u.col});
+    neighbors.forEach(v => {
+      var to_v_now = dist[v.row][v.col] || MSI;
+      var to_v_alt = to_u + matrix[v.row][v.col];
+      if (to_v_alt < to_v_now) {
+        dist[v.row][v.col] = to_v_alt;
+        prev[v.row][v.col] = u;
+      }
+    });
+  }
+
+  var min = dist.reduce((m, e) => Math.min(m, e[e.length - 1]), MSI);
+
+  return min;
+
 }
 
-var min = dist.reduce((m, e) => Math.min(m, e[e.length - 1]), MSI);
-
-console.log(min);
+console.log(dijkstra({row: 1, col: 0}));
