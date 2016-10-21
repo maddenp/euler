19d29119326ffe2bfda18e6533b6529f0fc9bed7
Paul Madden <maddenp@colorado.edu>
2016-10-21 15:03:33 -0600

    082: Better idea from forum, Chromebook time: 0.175s

--- p/js/082.js
+++ p/js/082.js
@@ -5,72 +5,29 @@
 
 const MSI = Number.MAX_SAFE_INTEGER;
 
-const matrix = require('fs').readFileSync('082.dat', 'utf8').trim().split('\n').map(x => x.split(',').map(s => parseInt(s)));
-
 // const matrix = [[131, 673, 234, 103,  18],
 //                 [201,  96, 342, 965, 150],
 //                 [630, 803, 746, 422, 111],
 //                 [537, 699, 497, 121, 956],
 //                 [805, 732, 524,  37, 331]];
 
-const findmin = (Q, dist) => {
-  var min_d = MSI;
-  var min_i = undefined;
-  for (var i = 0; i < Q.length; i++) {
-    var d = dist[Q[i].row][Q[i].col]
-    if (d < min_d) {
-      min_d = d;
-      min_i = i;
-    }
-  }
-  return min_i;
-};
+const matrix = require('fs').readFileSync('082.dat', 'utf8').trim().split('\n').map(x => x.split(',').map(s => parseInt(s)));
 
-const dijkstra = source => {
+const costs = matrix.reduce((m, e) => (m.push(e[0]), m), []);
 
-  // https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+// Thanks to hbf Project Eueler forum, post at Sat, 30 Jun 2007, 01:01
 
-  const dist = [];
-  const prev = [];
-  const Q = [];
-
+for (var col = 1; col < matrix[0].length; col++) {
+  var x = matrix.reduce((m, e) => (m.push(e[col]), m), []);
   for (var row = 0; row < matrix.length; row++) {
-    dist[row] = [];
-    prev[row] = [];
-    for (var col = 0; col < matrix[0].length; col++) {
-      dist[row][col] = MSI;
-      prev[row][col] = undefined;
-      Q.push({row: row, col: col});
-    }
+    costs[row] += matrix[row][col];
   }
-
-  dist[source.row][source.col] = matrix[source.row][source.col];
-
-  while (Q.length > 0) {
-
-    var u = Q.splice(findmin(Q, dist), 1)[0];
-    var to_u = dist[u.row][u.col];
-    var neighbors = []
-
-    if (u.row < dist.length - 1) neighbors.push({row: u.row + 1, col: u.col});
-    if (u.col < dist[u.row].length - 1) neighbors.push({row: u.row, col: u.col + 1});
-    if (u.row > 0) neighbors.push({row: u.row - 1, col: u.col});
-
-    neighbors.forEach(v => {
-      var to_v_now = dist[v.row][v.col] || MSI;
-      var to_v_alt = to_u + matrix[v.row][v.col];
-      if (to_v_alt < to_v_now) {
-        dist[v.row][v.col] = to_v_alt;
-        prev[v.row][v.col] = u;
-      }
-    });
-
+  for (row = 1; row < matrix.length; row++) {
+    costs[row] = Math.min(costs[row], x[row] + costs[row - 1]);
   }
-
-  var min = dist.reduce((m, e) => Math.min(m, e[e.length - 1]), MSI);
-
-  return min;
-
+  for (row = matrix.length - 2; row >= 0; row--) {
+    costs[row] = Math.min(costs[row], x[row] + costs[row + 1]);
+  }
 }
 
-console.log(matrix.reduce((m, _, i) => Math.min(m, dijkstra({row: i, col:0 })), MSI));
+console.log(Math.min.apply(null, costs));
