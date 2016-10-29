d199cc199427fe785b860e2e9c1f83f3c1625705
Paul Madden <maddenp@colorado.edu>
2016-10-29 14:42:15 -0600

    084: Work on simulator

--- p/js/084.js
+++ p/js/084.js
@@ -3,46 +3,61 @@
 
 "use strict";
 
-const cc = {2: true, 17: true, 33: true};
-const ch = {7: true, 22: true, 36: true};
+const pm = require('./pm');
+
+const next = a => () => {
+  var n = square;
+  while (++n) if (a.indexOf(n = n % 40) !== -1) return n;
+};
+
+const back3 = () => (square + 40 - 3) % 40;
+const c1 = () => 11;
+const cc_squares = {2: true, 17: true, 33: true};
+const ch_squares = {7: true, 22: true, 36: true};
 const die = 6;
 const die_max = Math.floor(die);
 const die_min = Math.ceil(1);
 const die_scale = die_max - die_min + 1
+const e3 = () => 24;
 const g2j = 30;
-const jail = 10;
-
+const go = () => 0;
+const h2 = () => 39;
+const histogram = [1];
+const jail = () => 10;
+const limit = 1000000;
+const nextr = next([5, 15, 25, 35]);
+const nextu = next([12, 28]);
+const r1 = () => 5;
 const roll = () => Math.floor(Math.random() * die_scale) + die_min;
+
+var cc = Array(14);
+var ch = Array(6);
+var doubles = 0;
+var square = 0;
 
 const move = () => {
   const roll1 = roll(die);
   const roll2 = roll(die);
-//   if (roll1 === roll2) doubles++;
-//   if (doubles === 3) {
-//     console.log('triple doubles');
-//     next = jail;
-//     doubles = 0;
-//   }
+  if (roll1 === roll2) doubles++;
+  if (doubles === 3) {
+    doubles = 0;
+    return jail();
+  }
   var next = (square + roll1 + roll2) % 40;
-//   if (next === g2j) {
-//     console.log('g2j');
-//     next = jail;
-//   }
+  if (next === g2j) {
+    return jail();
+  }
   return next;
 };
 
-const histogram = [];
-for (var i = 0; i < 40; i++) histogram[i] = 0;
-histogram[0] = 1;
+cc = pm.array_shuffle(cc.concat([go, jail]));
+ch = pm.array_shuffle(ch.concat([c1, e3, go, h2, jail, r1, nextr, nextr, nextu, back3]));
 
-var doubles = 0;
-var square = 0;
-
-const limit = 1000000;
+for (var i = 1; i < 40; i++) histogram[i] = 0;
 
 for (var i = 0; i < limit; i++) {
   square = move(square);
   histogram[square]++;
 }
 
-console.log(histogram.map(x => x/limit));
+console.log(histogram.map(x => x/limit*100));
