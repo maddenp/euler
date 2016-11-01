7793f65cdf304bbaa25a1fb813fe0f024b5bb36a
Paul Madden <maddenp@colorado.edu>
2016-10-31 21:08:50 -0600

    085: Cleanup

--- p/js/085.js
+++ p/js/085.js
@@ -3,14 +3,13 @@
 
 "use strict";
 
-const s = n => (n * (n + 1)) / 2;
+const s = n => (n * (n + 1)) / 2; // n choose 2
 
 var area;
 var min_diff = Number.MAX_SAFE_INTEGER;
 
 for (var rows = 1; rows <= 100; rows++) {
   for (var cols = 1; cols <= rows; cols++) {
-//     var rectangles = (rows * (rows + 1) / 2) * (cols * (cols + 1) / 2);
     var rectangles = s(rows) * s(cols);
     var diff = Math.abs(2000000 - rectangles);
     if (diff < min_diff) {
