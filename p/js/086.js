ccf1ba23ea9a7fd0f9bac711c75e5eda5b1a2160
Paul Madden <maddenp@colorado.edu>
2016-11-06 12:45:01 -0700

    086: More rigorous, no faster

--- p/js/086.js
+++ p/js/086.js
@@ -3,18 +3,37 @@
 
 "use strict";
 
-const M = 1818
-var sum = 0;
+const f = (a, b, c) => {
+  var len1 = Math.sqrt((a + b) * (a + b) + c * c);
+  var len2 = Math.sqrt((a + c) * (a + c) + b * b);
+  var minlen = len1 < len2 ? len1 : len2;
+  if (Math.floor(minlen) === minlen) ++sum;
+};
 
-for (var a = 1; a <= M; a++) {
-  for (var b = a; b <= M; b++) {
-    for (var c = b; c <= M; c++) {
-      var len1 = Math.sqrt((a + b) * (a + b) + c * c);
-      var len2 = Math.sqrt((a + c) * (a + c) + b * b);
-      var minlen = len1 < len2 ? len1 : len2;
-      if (Math.floor(minlen) === minlen) ++sum;
+const init = x => {
+  for (var a = 1; a <= x; a++) {
+    for (var b = a; b <= x; b++) {
+      for (var c = b; c <= x; c++) f(a, b, c);
     }
   }
-}
+};
 
-console.log(M);
+const incr = x => {
+  for (var a = x; a <= x; a++) {
+    for (var b = 1; b <= x; b++) {
+      for (var c = 1; c <= x; c++) f(a, b, c);
+    }
+  }
+};
+
+var sum = 0;
+const M = 1818;
+const limit = 1000000;
+init(M - 1);
+const lo = sum;
+incr(M - 0);
+const hi = sum;
+
+if (lo < limit && hi >= limit) {
+  console.log(M);
+}
