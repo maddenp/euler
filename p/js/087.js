5798e1b1d2e9912490e3216c1ada3a3be87b3e2c
Paul Madden <maddenp@colorado.edu>
2016-12-19 20:42:29 -0700

    087: Small optimization

--- p/js/087.js
+++ p/js/087.js
@@ -13,10 +13,12 @@ const squares = pm.primes(2, Math.sqrt(limit)).map(n => n * n);
 const cubes = pm.primes(2, Math.cbrt(limit)).map(n => n * n *n);
 const quarts = pm.primes(2, Math.sqrt(Math.sqrt(limit))).map(n => n * n * n *n);
 
-for (var i = 0; i < squares.length; i++) {
+for (var i = 0; i < quarts.length; i++) {
   for (var j = 0; j < cubes.length; j++) {
-    for (var k = 0; k < quarts.length; k++) {
-      var sum = squares[i] + cubes[j] + quarts[k];
+    var partial = quarts[i] + cubes[j];
+    if (partial > limit) continue;
+    for (var k = 0; k < squares.length; k++) {
+      var sum = partial + squares[k];
       if (sum < limit) ok.add(sum);
     }
   }
