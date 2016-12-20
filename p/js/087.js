84c44b84dce1956db44bb152aa314be35809ea30
Paul Madden <maddenp@colorado.edu>
2016-12-19 20:14:43 -0700

    087: Solution & test

---
+++ p/js/087.js
@@ -0,0 +1,24 @@
+/* jshint node: true */
+/* jshint esversion: 6 */
+
+"use strict";
+
+const pm = require('./pm');
+
+const limit = 50000000;
+const ok = {};
+
+const squares = pm.primes(2, Math.sqrt(limit)).map(n => n * n);
+const cubes = pm.primes(2, Math.cbrt(limit)).map(n => n * n *n);
+const quarts = pm.primes(2, Math.sqrt(Math.sqrt(limit))).map(n => n * n * n *n);
+
+for (var i = 0; i < squares.length; i++) {
+  for (var j = 0; j < cubes.length; j++) {
+    for (var k = 0; k < quarts.length; k++) {
+      var sum = squares[i] + cubes[j] + quarts[k];
+      if (sum < limit) ok[sum] = true;
+    }
+  }
+}
+
+console.log(Object.keys(ok).length);
