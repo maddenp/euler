8a8b1c1bdc561b5c1d00b02aff6deceb3e8123a0
Paul Madden <maddenp@colorado.edu>
2016-12-25 11:43:15 -0700

    088: k=12 works, still slow

--- p/js/088.js
+++ p/js/088.js
@@ -5,18 +5,29 @@
 
 const pm = require('./pm');
 
-const f = (a0, s0, p0) => {
+const g = (ns, n) => {
+  const h = Object.assign({}, ns);
+  h[n]--;
+  if (h[n] === 0) delete h[n];
+  n++;
+  h[n] = h[n] ? h[n] + 1 : 1;
+  return h;
+};
+
+const f = (s0, p0, ns) => {
   const candidates = [];
   const q = [];
   var s1 = s0 + 1;
-  for (let n of new Set(a0)) {
-    var a1 = a0.slice();
-    a1[a1.indexOf(n)]++;
-    var p1 = p0 / n * (n + 1);
+  Object.keys(ns).forEach(n0 => {
+    n0 = parseInt(n0);
+    var p1 = p0 / n0 * (n0 + 1);
     var d = s1 - p1;
     if (d === 0) candidates.push(s1);
-    if (d > 0) q.push([a1, s1, p1]);
-  }
+    if (d > 0) {
+      var z = g(ns, n0);
+      q.push([s1, p1, z]);
+    }
+  });
   if (candidates.length === 0) {
     q.forEach(x => {
       let c = f(x[0], x[1], x[2]);
@@ -30,10 +41,8 @@ const limit = 12;
 const mpsns = new Set([]); // minimal product-sum numbers
 
 for (var k = 2; k <= limit; k++) {
-  let a = [2];
-  for (var i = 1; i < k; i++) a.push(1);
-  var x = f(a, k + 1, 2);
-  mpsns.add(x);
+  var mpsn = f(k + 1, 2, {1: k - 1, 2: 1});
+  mpsns.add(mpsn);
 }
 
 var sum = 0;
