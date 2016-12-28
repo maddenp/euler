29c2ee06b8c7510e59133d3a68c549b05c640b9c
Paul Madden <maddenp@colorado.edu>
2016-12-28 15:07:02 -0700

    088: Eliminate unnecessary function

--- p/js/088.js
+++ p/js/088.js
@@ -5,27 +5,25 @@
 
 const pm = require('./pm');
 
-const g = (m, max, x, p, s, c) => {
+const f = (m, max, x, p, s, c) => {
   for (var y = 2; y <= x; y++) {
     var p1 = p * y;
     var s1 = s + y - 1;
     if (p1 === s1) c.add(p1);
     if (p1 > s1 || p1 > max || s1 > max) break;
-    if (m > 0) g(m - 1, max, y, p1, s1, c);
+    if (m > 0) f(m - 1, max, y, p1, s1, c);
   }
-};
-
-const f = (n) => {
-  const c = new Set([]);
-  const max = 2 * n;
-  const m = Math.floor(Math.log2(max));
-  for (var x = 2; x <= n; x++) g(m - 2, max, x, x, x + n - 1, c);
-  return pm.array_min(Array.from(c));
 };
 
 const limit = 12000;
 const mpsns = new Set([]); // minimal product-sum numbers
 
-for (var k = 2; k <= limit; k++) mpsns.add(f(k));
+for (var n = 2; n <= limit; n++) {
+  const c = new Set([]);
+  const max = 2 * n;
+  const m = Math.floor(Math.log2(max));
+  for (var x = 2; x <= n; x++) f(m - 2, max, x, x, x + n - 1, c);
+  mpsns.add(pm.array_min(Array.from(c)));
+}
 
 console.log(pm.array_sum(Array.from(mpsns)));
