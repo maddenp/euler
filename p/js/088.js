eed1f5de9c6a1d6a67caedfc7c0e803f02e14960
Paul Madden <maddenp@colorado.edu>
2016-12-28 14:14:44 -0700

    088: Refactoring

--- p/js/088.js
+++ p/js/088.js
@@ -5,33 +5,27 @@
 
 const pm = require('./pm');
 
-const g = (m, c, x, p, s, candidates) => {
-  if (m < 0) return false;
+const g = (m, max, x, p, s, c) => {
   for (var y = 2; y <= x; y++) {
     var p1 = p * y;
     var s1 = s + y - 1;
-    if (p1 === s1) candidates.add(p1);
-    if (p1 > s1 || p1 > c || s1 > c) return false;
-    g(m - 1, c, y, p1, s1, candidates);
+    if (p1 === s1) c.add(p1);
+    if (p1 > s1 || p1 > max || s1 > max) break;
+    if (m > 0) g(m - 1, max, y, p1, s1, c);
   }
 };
 
 const f = (n) => {
-  const candidates = new Set([]);
-  const c = 2 * n;
-  const m = Math.floor(Math.log2(c));
-  for (var x = 2; x <= n; x++) {
-    g(m - 2, c, x, x, x + n - 1, candidates);
-  }
-  return pm.array_min(Array.from(candidates));
+  const c = new Set([]);
+  const max = 2 * n;
+  const m = Math.floor(Math.log2(max));
+  for (var x = 2; x <= n; x++) g(m - 2, max, x, x, x + n - 1, c);
+  return pm.array_min(Array.from(c));
 };
 
 const limit = 12000;
 const mpsns = new Set([]); // minimal product-sum numbers
 
-for (var k = 2; k <= limit; k++) {
-  var mpsn = f(k);
-  mpsns.add(mpsn);
-}
+for (var k = 2; k <= limit; k++) mpsns.add(f(k));
 
 console.log(pm.array_sum(Array.from(mpsns)));
