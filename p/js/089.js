ce703b06ace7b788fe0925183701ff8070c3fc3f
Paul Madden <maddenp@colorado.edu>
2016-12-30 10:20:14 -0700

    089: DRY out replacement pattern

--- p/js/089.js
+++ p/js/089.js
@@ -7,23 +7,13 @@ var before = 0;
 var after = 0;
 
 require('fs').readFileSync('089.dat', 'utf8').split('\n').forEach(s => {
-
   before += s.length;
-
-  s = s.replace(/IIII/, 'IV');
-  s = s.replace(/VIV/, 'IX');
-  s = s.replace(/VIIII/, 'IX');
-
-  s = s.replace(/XXXX/, 'XL');
-  s = s.replace(/LXL/, 'XC');
-  s = s.replace(/LXXXX/, 'XC');
-
-  s = s.replace(/CCCC/, 'CD');
-  s = s.replace(/DCD/, 'CM');
-  s = s.replace(/DCCCC/, 'CM');
-
+  [['I', 'V', 'X'], ['X', 'L', 'C'], ['C', 'D', 'M']].forEach(x => {
+    s = s.replace(new RegExp(`${x[0]}{4}`), `${x[0]}${x[1]}`);
+    s = s.replace(new RegExp(`${x[1]}${x[0]}${x[1]}`), `${x[0]}${x[2]}`);
+    s = s.replace(new RegExp(`${x[1]}${x[0]}{4}`), `${x[0]}${x[2]}`);
+  });
   after += s.length;
-
 });
 
 console.log(before - after);
