02550be217154988f2e27e1a6ac99353c5a4d040
Paul Madden <maddenp@colorado.edu>
2016-12-30 10:53:03 -0700

    089: Prettier code

--- p/js/089.js
+++ p/js/089.js
@@ -6,14 +6,15 @@
 var before = 0;
 var after = 0;
 
-require('fs').readFileSync('089.dat', 'utf8').split('\n').forEach(s => {
-  before += s.length;
-  [['I', 'V', 'X'], ['X', 'L', 'C'], ['C', 'D', 'M']].forEach(x => {
-    s = s.replace(new RegExp(`${x[0]}{4}`), `${x[0]}${x[1]}`);
-    s = s.replace(new RegExp(`${x[1]}${x[0]}${x[1]}`), `${x[0]}${x[2]}`);
-    s = s.replace(new RegExp(`${x[1]}${x[0]}{4}`), `${x[0]}${x[2]}`);
+require('fs').readFileSync('089.dat', 'utf8').split('\n').forEach(rn => {
+  before += rn.length;
+  ['IVX', 'XLC', 'CDM'].forEach(x => {
+    var s = x[0], m = x[1], l = x[2];
+    rn = rn.replace(new RegExp(`${s}{4}`), `${s}${m}`);
+    rn = rn.replace(new RegExp(`${m}${s}${m}`), `${s}${l}`);
+    rn = rn.replace(new RegExp(`${m}${s}{4}`), `${s}${l}`);
   });
-  after += s.length;
+  after += rn.length;
 });
 
 console.log(before - after);
