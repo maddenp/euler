36af1102acd9b4cdb672153ea97d4fa93bfcdcc3
Paul Madden <maddenp@colorado.edu>
2017-01-02 13:01:00 -0700

    090: Simplify choose() using slice()

--- p/js/090.js
+++ p/js/090.js
@@ -3,14 +3,14 @@
 
 "use strict";
 
-const choose = (a, k, i) => {
-  if (k === 1) return a.slice(i);
+const choose = (a, k) => {
+  if (k === 1) return a;
   let b = [];
-  for (let j = i; j < a.length; j++) {
-    choose(a, k - 1, j + 1).forEach(x => b.push([a[j]].concat(x)));
+  for (let i = 0; i < a.length; i++) {
+    choose(a.slice(i + 1), k - 1).forEach(x => b.push([a[i]].concat(x)));
   }
   return b;
 };
 
-const combos = choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 6, 0);
+const combos = choose([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 6);
 console.log(combos.length);
