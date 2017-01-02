cf0d5e90005659315e14336a9acce9453d92e4ec
Paul Madden <maddenp@colorado.edu>
2017-01-02 12:46:16 -0700

    090: WIP, simplify combos fn

--- p/js/090.js
+++ p/js/090.js
@@ -4,23 +4,13 @@
 "use strict";
 
 const f = (a, i, k) => {
-//   console.log(`entry: a=${JSON.stringify(a)} i=${i} k=${k}`);
   if (k === 1) return a.slice(i);
   const b = [];
   for (var j = i; j < a.length; j++) {
-//     console.log(`j=${j}`);
-    var head = [a[j]];
-//     console.log(`head=${JSON.stringify(head)}`);
-    var tail = f(a, j + 1, k - 1);
-//     console.log(`tail=${JSON.stringify(tail)}`);
-    tail.forEach(x => {
-      var y = head.concat(x);
-//       console.log(`y=${JSON.stringify(y)}`);
-      b.push(y);
-    });
+    f(a, j + 1, k - 1).forEach(x => b.push([a[j]].concat(x)));
   }
-//   console.log(`b=${JSON.stringify(b)}`);
   return b;
 };
 
-console.log(f([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 6).length);
+const combos = f([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 6);
+console.log(combos.length);
