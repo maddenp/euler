3fb60257ca55e51173a9fd560f6a5ea1ee334efe
Paul Madden <maddenp@colorado.edu>
2017-01-08 10:54:34 -0700

    091: Solution + test, chromebook time 0.235s

--- p/js/091.js
+++ p/js/091.js
@@ -3,22 +3,43 @@
 
 "use strict";
 
-const limit = 9;
+const limit = 50;
 
 var total = 0;
 
-for (var x = 1; x <= limit; x++) {
-  for (var y = x; y <= limit; y++) {
-    total += x === y ? 3 : 6;
-    for (var i = 1; i <= y / 2; i++) {
-      var j = y - i;
-      var asq = x * x + i * i;
-      var bsq = x * x + j * j;
-      var csq = y * y;
-      if (asq + bsq !== csq) continue;
-      var inner = i === y / 2 ? 1 : 2;
-      total += x === y ? inner : inner * 2;
+const sqd = (x0, y0, x1, y1) => (x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0);
+
+for (var w = 1; w <= limit; w++) {
+  for (var h = w; h <= limit; h++) {
+    var count = w === h ? 3 : 6;
+    var y0 = h;
+    var x1 = w;
+    for (var x0 = 0; x0 <= w; x0++) {
+      for (var y1 = 1; y1 < h; y1++) {
+        var sqd1 = sqd(x0, y0, x1, y1);
+        var sqd2 = sqd(x1, y1,  0,  0);
+        var sqd3 = sqd(x0, y0,  0,  0);
+        var maxd = Math.max(sqd1, sqd2, sqd3);
+        if (maxd === sqd1) {
+          var asq = sqd2;
+          var bsq = sqd3;
+          var csq = sqd1;
+        } else if (maxd === sqd2) {
+          var asq = sqd1;
+          var bsq = sqd3;
+          var csq = sqd2;
+        } else if (maxd === sqd3) {
+          var asq = sqd1;
+          var bsq = sqd2;
+          var csq = sqd3;
+        }
+        if (asq + bsq === csq) {
+          count += 1;
+          if (w !== h) count += 1;
+        }
+      }
     }
+    total += count;
   }
 }
 
