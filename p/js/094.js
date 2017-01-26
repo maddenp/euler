9c134f892da6660743a70dec1826f518bfbb222b
Paul Madden <maddenp@colorado.edu>
2017-01-26 10:08:58 -0700

    094: Fixes for jshint

--- p/js/094.js
+++ p/js/094.js
@@ -5,7 +5,7 @@
 
 const pm = require('./pm');
 
-var sum = 0;;
+var sum = 0;
 
 const exclude = (T) => T[0] + T[1] + T[2] > 1000000000;
 
@@ -14,7 +14,7 @@ const accept = (T) => {
   var a = T[2];
   var b = T[0] * 2;
   if (Math.abs(a - b) === 1) sum += 2 * a + b;
-}
+};
 
 pm.pythagorean_triples(exclude, accept, true);
 
