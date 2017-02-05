8611993fa6b3e18f4549d96d6696acf5b522b3e2
Paul Madden <maddenp@colorado.edu>
2017-02-05 10:36:03 -0700

    095: Simplifications

--- p/js/095.js
+++ p/js/095.js
@@ -25,7 +25,7 @@ const f = (n, o) => {
 
 const limit = 1e6;
 const sopd = [];
-const sums = {1: 0};
+const sums = [0, 0];
 
 var maxlen = 0;
 var minval = 0;
@@ -40,11 +40,9 @@ for (var n = 2; n <= limit; n++) {
 
 for (var n = 2; n <= limit; n++) {
   if (sums[n]) continue;
-  var m = n;
-  while (!sums[m]) {
-    var s = sopd[m];
-    sums[m] = s > limit ? 0 : s;
-    if (sums[m] === 0) break;
+  while (!sums[n]) {
+    sums[n] = sopd[n] > limit ? 0 : sopd[n];
+    if (sums[n] === 0) break;
   }
 }
 
