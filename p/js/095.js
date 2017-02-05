af6f036f49a61cdf77e78c96e647d795d62dc7d1
Paul Madden <maddenp@colorado.edu>
2017-02-05 10:10:44 -0700

    095: Better sum-of-proper-divisors algo, cb time: 2.108s

--- p/js/095.js
+++ p/js/095.js
@@ -29,11 +29,19 @@ const sums = {1: 0};
 var maxlen = 0;
 var minval = 0;
 
+const sopd = [];
+for (var n = 1; n <= limit; n++) sopd[n] = 1;
+for (var n = 2; n <= limit; n++) {
+  for (var m = n + n; m <= limit; m += n) {
+    sopd[m] += n;
+  }
+}
+
 for (var n = 2; n <= limit; n++) {
   if (sums[n]) continue;
   var m = n;
   while (!sums[m]) {
-    var s = pm.sum_of_proper_divisors(m);
+    var s = sopd[m];
     sums[m] = s > limit ? 0 : s;
     if (sums[m] === 0) break;
   }
