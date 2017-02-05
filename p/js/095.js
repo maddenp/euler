506fd49adf76fd81f78744a4a7c884f9b00ff3c9
Paul Madden <maddenp@colorado.edu>
2017-02-05 09:59:10 -0700

    095: Reorg

--- p/js/095.js
+++ p/js/095.js
@@ -5,19 +5,6 @@
 
 const pm = require('./pm');
 
-const limit = 1000000;
-const sums = {1: 0};
-
-for (var n = 2; n <= limit; n++) {
-  if (sums[n]) continue;
-  var m = n;
-  while (!sums[m]) {
-    var s = pm.sum_of_proper_divisors(m);
-    sums[m] = s > limit ? 0 : s;
-    if (sums[m] === 0) break;
-  }
-}
-
 const f = (n, o) => {
   if (sums[n] === 0) {
     o.len = 0;
@@ -36,8 +23,21 @@ const f = (n, o) => {
   }
 };
 
+const limit = 1000000;
+const sums = {1: 0};
+
 var maxlen = 0;
 var minval = 0;
+
+for (var n = 2; n <= limit; n++) {
+  if (sums[n]) continue;
+  var m = n;
+  while (!sums[m]) {
+    var s = pm.sum_of_proper_divisors(m);
+    sums[m] = s > limit ? 0 : s;
+    if (sums[m] === 0) break;
+  }
+}
 
 for (var n = 2; n <= limit; n++) {
   if (sums[n] === 0) continue;
