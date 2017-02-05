de8552c3a3aa518594743b5adc197c33dbe39568
Paul Madden <maddenp@colorado.edu>
2017-02-05 10:41:07 -0700

    095: Simplifications, cb time: 1.914s

--- p/js/095.js
+++ p/js/095.js
@@ -24,25 +24,16 @@ const f = (n, o) => {
 };
 
 const limit = 1e6;
-const sopd = [];
 const sums = [0, 0];
 
 var maxlen = 0;
 var minval = 0;
 
-for (var n = 2; n <= limit; n++) sopd[n] = 1;
+for (var n = 2; n <= limit; n++) sums[n] = 1;
 
 for (var n = 2; n <= limit; n++) {
   for (var m = n + n; m <= limit; m += n) {
-    sopd[m] += n;
-  }
-}
-
-for (var n = 2; n <= limit; n++) {
-  if (sums[n]) continue;
-  while (!sums[n]) {
-    sums[n] = sopd[n] > limit ? 0 : sopd[n];
-    if (sums[n] === 0) break;
+    sums[m] = sums[m] + n > limit ? 0 : sums[m] + n;
   }
 }
 
