faf91e2f8cc2fd9e4cb8ee23197a68a56c3c8fb8
Paul Madden <maddenp@colorado.edu>
2017-02-07 07:38:10 -0700

    096: Parse grids

--- p/js/096.js
+++ p/js/096.js
@@ -5,3 +5,18 @@
 
 const pm = require('./pm');
 
+const grids = [];
+
+var i = 0;
+var grid = [];
+
+require('fs').readFileSync('096.dat', 'utf8').split('\n').forEach(line => {
+  if (i !== 0) {
+    grid.push(line.split(''));
+    if (i === 9) {
+      grids.push(grid);
+      grid = [];
+    }
+  }
+  i = (i + 1) % 10;
+});
