ac0ec9becfd75eaae3d77b78fab882edf693ff4c
Paul Madden <maddenp@colorado.edu>
2017-02-12 16:36:50 -0700

    096: Clean up, fall back

--- p/js/096.js
+++ p/js/096.js
@@ -14,11 +14,11 @@ const good = (board) => {
     for (var r = 0; r < 9; r++) sum += board[r][c];
     if (sum !== 45) return false;
   }
-  for (var sq_r = 0; sq_r < 7; sq_r += 3) {
-    for (var sq_c = 0; sq_c < 7; sq_c += 3) {
+  for (var srq = 0; srq < 7; srq += 3) {
+    for (var sqc = 0; sqc < 7; sqc += 3) {
       var sum = 0;
-      for (r = sq_r; r < sq_r + 3; r++) {
-        for (c = sq_c; c < sq_c + 3; c++) {
+      for (r = srq; r < srq + 3; r++) {
+        for (c = sqc; c < sqc + 3; c++) {
           sum += board[r][c];
         }
       }
@@ -45,13 +45,7 @@ const load = () => {
   return boards;
 };
 
-const copy = (board) => {
-  const b = [];
-  board.forEach(row => b.push(row.slice()));
-  return b;
-};
-
-const makeWork = () => {
+const make = () => {
   const work = [];
   for (var r = 0; r < 9; r++) {
     work[r] = [];
@@ -62,11 +56,11 @@ const makeWork = () => {
   return work;
 };
 
-const printBoard = (board) => {
+const printb = (board) => {
   board.forEach(row => console.log(JSON.stringify(row)));
 };
 
-const printWork = (work) => {
+const printw = (work) => {
   const f = s => {
     var a = '';
     for (var n = 1; n <= 9; n++) a = `${a} ${s.has(n) ? n : 0}`;
@@ -76,12 +70,8 @@ const printWork = (work) => {
   work.forEach(row => console.log(row.reduce((m, e) => m += f(e), '')));
 };
 
-const solveBoard = (board) => {
-  const work = makeWork();
-
-  //   printWork(work);
-  //   console.log('---');
-
+const solve = (board) => {
+  const work = make();
   var unsolved = true;
   var unchanged = true;
   while (unsolved) {
@@ -94,76 +84,34 @@ const solveBoard = (board) => {
           work[r][c] = new Set([board[r][c]]);
           continue;
         }
-        for (var rr = 0; rr < 9; rr++) {
-          if (board[rr][c]) {
-            work[r][c].delete(board[rr][c]);
-            if (work[r][c].size === 0) return false;
-          }
-        }
-        for (var cc = 0; cc < 9; cc++) {
-          if (board[r][cc]) {
-            work[r][c].delete(board[r][cc]);
-            if (work[r][c].size === 0) return false; 
-          }
-        }
-        var sq_r = Math.floor(r/3) * 3;
-        var sq_c = Math.floor(c/3) * 3;
-        for (var rr = sq_r; rr < sq_r + 3; rr++) {
-          for (var cc = sq_c; cc < sq_c + 3; cc++) {
-            if (board[rr][cc]) {
-              work[r][c].delete(board[rr][cc]);
-              if (work[r][c].size === 0) return false;
-            }
-          }
+        for (var rr = 0; rr < 9; rr++) work[r][c].delete(board[rr][c]);
+        for (var cc = 0; cc < 9; cc++) work[r][c].delete(board[r][cc]);
+        var sqr = Math.floor(r/3) * 3;
+        var sqc = Math.floor(c/3) * 3;
+        for (var rr = sqr; rr < sqr + 3; rr++) {
+          for (var cc = sqc; cc < sqc + 3; cc++) work[r][c].delete(board[rr][cc]);
         }
         if (work[r][c].size === 1) {
-          board[r][c] = work[r][c].values().next().value;
+          board[r][c] = Array.from(work[r][c])[0];
           unchanged = false;
         }
       }
     }
-
-//     printBoard(board);
-//     printWork(work);
-//     console.log('---');
-
-    if (unchanged) {
-//       console.log('unchanged');
-      out: for (var r = 0; r < 9; r++) {
-        for (var c = 0; c < 9; c++) {
-          var choices = Array.from(work[r][c]);
-          while (choices.length > 1) {
-//             console.log(`choices at (${r},${c}): ${choices}`);
-            var pick = choices.pop();
-//             console.log(`picked ${pick} (choices remaining: ${JSON.stringify(choices)}`);
-            var b = copy(board);
-            b[r][c] = pick;
-//             console.log('Recursing with board:');
-//             printBoard(b);
-//             console.log('---');
-            var soln = solveBoard(b);
-//             console.log('Back from recursion');
-            if (soln) return soln;
-          }
-        }
-      }
-      break; // are both unchanged and unsolved necessary?
-    }
-
+    printw(work);
+    printb(board);
+    if (unchanged) break;
     unchanged = true;
   }
-
   if (good(board)) return board;
   return false;
 };
 
 const boards = load();
-var board = boards[1];
-// printBoard(board);
-// console.log('---');
-var soln = solveBoard(board);
+var board = boards[0];
+printb(board);
+var soln = solve(board);
 if (soln) {
-  printBoard(soln);
+  printb(soln);
 } else {
   console.log('FAIL');
 }
