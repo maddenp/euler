f8683a67e1bdb6c20425c8ad4713b02a44f6cfc8
Paul Madden <maddenp@colorado.edu>
2017-02-15 14:23:24 -0700

    098: Optimizations, cb time 1.755s

--- p/js/098.js
+++ p/js/098.js
@@ -5,6 +5,8 @@
 
 const pm = require('./pm');
 
+const lengths = new Set();
+const pairs = [];
 const words = {};
 
 var lenmin = Number.MAX_SAFE_INTEGER;
@@ -16,63 +18,62 @@ require('fs').readFileSync('098.dat', 'utf8').split(',').forEach(s => {
   if (len < lenmin) lenmin = len;
   if (len > lenmax) lenmax = len;
   if (!words[len]) words[len] = [];
-  var a = word.split('').sort();
-  var t = new Set(a);
-  words[len].push({s: word, a, t, k: Array.from(t)});
+  var letters = word.split('');
+  var letters_sorted = letters.slice().sort();
+  words[len].push({word, letters, letters_sorted});
 });
 
-const anagramsp = (a1, a2) => {
-  for (var i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) return false;
-  }
-  return true;
-};
-
-const getmaps = (x) => {
-  const a = [];
-  const maps = [];
-  for (var i = 0; i < x.t.size; i++) a.push(0);
-  for (var i = 0; i < Math.pow(10, x.t.size); i++) {
-    var t = new Set(a);
-    if (t.size === a.length) {
-      var keys = Array.from(x.t);
-      var map = {};
-      for (var j = 0; j < keys.length; j++) map[keys[j]] = a[j];
-      var b = x.s.split('').reduce((m, e) => `${m}${map[e]}`, '');
-      var bi = parseInt(b);
-      if (b[0] !== '0' && pm.is_square(bi)) {
-        maps.push(map);
+for (var n = lenmin; n <= lenmax; n++) {
+  for (let w of words[n]) {
+    if (w.seen) continue;
+    for (let x of words[n]) {
+      if (x.word === w.word) continue;
+      for (var i = 0; i < w.letters_sorted.length; i++) {
+        if (x.letters_sorted[i] !== w.letters_sorted[i]) break;
+      }
+      if (i === w.letters_sorted.length) {
+        pairs.push([w, x]);
+        x.seen = true;
+        lengths.add(w.letters_sorted.length);
       }
     }
-    pm.array_inc(a);
   }
-  return maps;
+};
+
+lenmax = pm.array_max(Array.from(lengths));
+const maxsquare = Math.pow(10, lenmax) - 1;
+const squares = {};
+for (var n = 1; n <= lenmax; n++) squares[n] = {};
+for (var n = 1; true; n++) {
+  var square = n * n;
+  if (square > maxsquare) break;
+  squares[pm.ndigits(square)][square] = true;
+}
+
+const check = (pair) => {
+  var max = 0;
+  var a = pair[0];
+  var b = pair[1];
+  var len = a.letters.length;
+  var candidates = Object.keys(squares[len]);
+  candidates.forEach(square => {
+    var letters = `${square}`.split(''); // n2a faster?
+    var uniques = Array.from(new Set(letters)); // do this once when creating squares?
+    if (uniques.length === len) {
+      var map = {};
+      for (var i = 0; i < len; i++) {
+        map[a.letters[i]] = uniques[i];
+      }
+      var b_alt = b.letters.reduce((m, e) => `${m}${map[e]}`, '');
+      var b_int = parseInt(b_alt);
+      if (squares[len][b_int]) {
+        max = Math.max(max, square, b_int);
+      }
+    }
+  });
+  return max;
 };
 
 var max = 0;
-
-for (var n = lenmin; n <= lenmax; n++) {
-  words[n].forEach(w => {
-    var anagrams = words[n].filter(x => x !== w && anagramsp(w.a, x.a));
-    if (anagrams.length !== 0) {
-      var maps = getmaps(w);
-      maps.forEach(map => {
-        var a = w.s.split('').reduce((m, e) => `${m}${map[e]}`, '');
-        var ai = parseInt(a);
-        anagrams.forEach(x => {
-          var b = x.s.split('').reduce((m, e) => `${m}${map[e]}`, '');
-          var bi = parseInt(b);
-          if (b[0] !== '0' && pm.is_square(bi)) {
-            var ci = Math.max(ai, bi);
-            if (ci > max) {
-              max = ci;
-//               console.log(max);
-            }
-          }
-        });
-      });
-    }
-  });
-};
-
+pairs.forEach(pair => max = Math.max(max, check(pair)));
 console.log(max);
